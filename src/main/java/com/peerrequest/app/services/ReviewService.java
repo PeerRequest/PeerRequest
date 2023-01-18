8d00f288571775fde48bc0387cf972e6a1956e3d
Nikola <ulsxl@student.kit.edu>
2023-01-18 13:18:54 +0100

    change to ZonedDateTime class

--- src/main/java/com/peerrequest/app/services/ReviewService.java
+++ src/main/java/com/peerrequest/app/services/ReviewService.java
@@ -1,10 +1,10 @@
 package com.peerrequest.app.services;
 
 import com.peerrequest.app.model.Review;
-import org.springframework.format.annotation.DateTimeFormat;
+import java.time.ZonedDateTime;
 
 public interface ReviewService<F, U> extends CrudService<Review, Review.ReviewSelector, F, U> {
-    void addMessage(long reviewID, long userID, DateTimeFormat timeStamp, String content);
+    void addMessage(long reviewID, long userID, ZonedDateTime timeStamp, String content);
 
     void deleteMessage(long commentID);
 
