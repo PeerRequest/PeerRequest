90053697356d1d7f5f692867ac2d3f7836ae2c5a
Nikola <ulsxl@student.kit.edu>
2023-01-18 17:00:57 +0100

    change RequestService interface

--- src/main/java/com/peerrequest/app/services/RequestService.java
+++ src/main/java/com/peerrequest/app/services/RequestService.java
@@ -1,5 +1,8 @@
 package com.peerrequest.app.services;
 
+import com.peerrequest.app.model.DirectRequestProcess;
+import com.peerrequest.app.model.Request;
+
 import java.util.List;
 
 public interface RequestService<F, U> extends CrudService<Request, Request.RequestSelector, F, U> {
