f8907b008505880d89128d212002d454070313db
Wil-Lee <mildt.michael@gmail.com>
2023-01-19 00:17:06 +0100

    + java doc + constructor + attribute visibility

--- src/main/java/com/peerrequest/app/model/Request.java
+++ src/main/java/com/peerrequest/app/model/Request.java
@@ -1,14 +1,52 @@
 package com.peerrequest.app.model;
 
+
 import lombok.Getter;
-import lombok.Setter;
 
-public class Request {
-    @Getter
-    @Setter
-    RequestSelector id;
+/**
+ * This abstract class represents a request.
+ *
+ * @author User3 Boban
+ * @author User5 Mildt
+ * @version 0.0.1
+ */
+public abstract class Request {
 
-    public record RequestSelector(long id) {
+    /**
+     * Constructor for a request.
+     *
+     * @param id          id of the request.
+     * @param reviewerID  id of the requested user.
+     */
+    public Request(RequestSelector id, User.UserSelector reviewerID) {
+        this.id = id;
+        this.reviewerID = reviewerID;
     }
 
+    @Getter
+    private final RequestSelector id;
+
+    @Getter
+    private final User.UserSelector reviewerID;
+
+    public record RequestSelector(long requestID) {
+    }
+
+    /**
+     * Represents the states of a request.
+     */
+    public enum RequestState {
+        /**
+         * Request state when a user accepted a request.
+         */
+        ACCEPTED,
+        /**
+         * Request state when a user declined a request.
+         */
+        DECLINED,
+        /**
+         * Request state when a user neither accepted nor declined a request.
+         */
+        PENDING;
+    }
 }
