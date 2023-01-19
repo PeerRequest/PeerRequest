e523e037a3a6ec502349990c1f1cee499351f277
Nikola <ulsxl@student.kit.edu>
2023-01-19 18:56:14 +0100

    change ID datatype

--- src/main/java/com/peerrequest/app/services/ReviewService.java
+++ src/main/java/com/peerrequest/app/services/ReviewService.java
@@ -2,13 +2,14 @@ package com.peerrequest.app.services;
 
 import com.peerrequest.app.model.Review;
 import com.peerrequest.app.model.User;
+import com.peerrequest.app.model.Messsage;
 
 import java.time.ZonedDateTime;
 
 public interface ReviewService<F, U> extends CrudService<Review, Review.ReviewSelector, F, U> {
     void addMessage(Review.ReviewSelector reviewID, User.UserSelector userID, ZonedDateTime timeStamp, String content);
 
-    void deleteMessage(long commentID);
+    void deleteMessage(Message.MessageSelector commentID);
 
     void notifyResearcherOfEdit(Review.ReviewSelector reviewID);
 
