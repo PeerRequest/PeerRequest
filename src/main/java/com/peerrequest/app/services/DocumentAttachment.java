062f427d2e7e2d29b9098ab4d8c0485379563830
upxee <upxee@student.kit.edu>
2023-01-19 15:34:53 +0100

    add DocumentAttachment.java

---
+++ src/main/java/com/peerrequest/app/services/DocumentAttachment.java
@@ -0,0 +1,17 @@
+package com.peerrequest.app.services;
+
+import com.peerrequest.app.model.Document;
+
+/**
+ * Interface for the document attached to objects.
+ * @param <P> the object the document is attached to
+ */
+public interface DocumentAttachment<P> {
+
+    /**
+     * returns the document attached to the selected object.
+     * @param selector the selected object
+     * @return the document attached to the selected object
+     */
+    Document document(P selector);
+}
