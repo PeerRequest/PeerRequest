6a60b8c4ccf3f3d01e766e6dfe3d3640324829d6
Wil-Lee <mildt.michael@gmail.com>
2023-01-19 20:22:33 +0100

    add bidding process controller with mock data

---
+++ src/main/java/com/peerrequest/app/api/BiddingProcessesController.java
@@ -0,0 +1,52 @@
+package com.peerrequest.app.api;
+
+import com.peerrequest.app.model.*;
+import org.springframework.web.bind.annotation.*;
+
+import java.time.ZonedDateTime;
+
+@RestController
+@RequestMapping
+public class BiddingProcessesController {
+
+    @GetMapping(value = "/categories/{categoryId}/biddingProcess", produces = "application/json")
+    public BiddingProcess putBiddingProcess(@PathVariable final long categoryId,
+                                            @RequestBody final BiddingProcess biddingProcess) {
+        return mockBiddingProcess();
+    }
+
+    @GetMapping(value = "/categories/{categoryId}/biddingProcess", produces = "application/json")
+    public BiddingProcess patchBiddingProcess(@PathVariable final long categoryId,
+                                              @RequestBody final BiddingProcess.BiddingProcessUpdater updater) {
+        mockBiddingProcess().setDeadline((updater.deadline()));
+        return mockBiddingProcess();
+    }
+
+    @GetMapping(value = "/categories/{categoryId}/biddingProcess", produces = "application/json")
+    public BiddingProcess getBiddingProcess(@PathVariable final long categoryId) {
+        return mockBiddingProcess();
+    }
+
+    @GetMapping(value = "/categories/{categoryId}/biddingProcess", produces = "application/json")
+    public void deleteBiddingProcess(@PathVariable final long categoryId) {
+
+    }
+
+    private BiddingProcess mockUp;
+
+    private BiddingProcess mockBiddingProcess() {
+
+        if (mockUp != null) {
+            return mockUp;
+        }
+
+        ZonedDateTime deadline = ZonedDateTime.now();
+        BiddingProcess.BiddingProcessSelector biddingProcessID = new BiddingProcess.BiddingProcessSelector(1);
+        Category.CategorySelector categoryID = new Category.CategorySelector(1);
+
+        this.mockUp = new BiddingProcess(biddingProcessID, categoryID, deadline);
+
+        return mockUp;
+    }
+
+}
