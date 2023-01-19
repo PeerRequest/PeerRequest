6a5863ebf5025b9a3e5bfa2ed258b5e0900e2789
Nikola <ulsxl@student.kit.edu>
2023-01-19 18:52:34 +0100

    change ID datatype

--- src/main/java/com/peerrequest/app/services/RequestService.java
+++ src/main/java/com/peerrequest/app/services/RequestService.java
@@ -1,24 +1,27 @@
 package com.peerrequest.app.services;
 
 import com.peerrequest.app.model.DirectRequestProcess;
+import com.peerrequest.app.model.Entry;
 import com.peerrequest.app.model.Request;
+import com.peerrequest.app.model.User;
 
 import java.util.List;
 
 public interface RequestService<F, U> extends CrudService<Request, Request.RequestSelector, F, U> {
 
-    void createDirectRequestProcess(long entryID, List<Long> userIDs, int openSlots);
+    void createDirectRequestProcess(Entry.EntrySelector entryID, List<User.UserSelector> userIDs, int openSlots);
 
     void setOpenSlots(int openSlots);
 
-    void claimOpenSlot(long directRequestProcessID, long userID);
+    void claimOpenSlot(
+            DirectRequestProcess.DirectRequestProcessSelector directRequestProcessID, User.UserSelector userID);
 
-    void acceptDirectRequest(long requestID);
+    void acceptDirectRequest(Request.RequestSelector requestID);
 
-    void declineDirectRequest(long requestID);
+    void declineDirectRequest(Request.RequestSelector requestID);
 
-    void declineBiddingRequest(long requestID);
+    void declineBiddingRequest(Request.RequestSelector requestID);
 
-    DirectRequestProcess getDirectRequestProcess(long entryID);
+    DirectRequestProcess getDirectRequestProcess(Entry.EntrySelector entryID);
 
 }
