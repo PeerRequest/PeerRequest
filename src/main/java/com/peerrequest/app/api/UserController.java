77771bd2d42a3b7b43e74cc388de0d3befe1cf73
upxee <upxee@student.kit.edu>
2023-01-20 10:34:20 +0100

    adjust style to match other controller

--- src/main/java/com/peerrequest/app/api/UserController.java
+++ src/main/java/com/peerrequest/app/api/UserController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
-import java.util.List;
+
 
 @RestController
 @RequestMapping
@@ -16,45 +16,50 @@ public class UserController {
     @GetMapping(value = "/users/{userId}/reviews", produces = "application/json")
     public List<Review> getReviews(@PathVariable int userId) {
         List<Review> reviewsList = new ArrayList<>();
-        reviewsList.add(mockReview());
+        reviewsList.add(this.mockUpReview);
         return reviewsList;
     }
 
     @GetMapping(value = "/users/{userId}/entries", produces = "application/json")
     public List<Entry> getEntries(@PathVariable int userId) {
         List<Entry> entryList = new ArrayList<>();
-        entryList.add(mockEntryOne());
-        entryList.add(mockEntryTwo());
+        entryList.add(this.mockUpEntryOne);
+        entryList.add(this.mockUpEntryTwo);
         return entryList;
     }
+
+    private User mockUpUser;
+    private Entry mockUpEntryOne;
+    private Entry mockUpEntryTwo;
+    private Review mockUpReview;
 
     //returns mock user
     private User mockUser() {
         User.UserSelector userId = new User.UserSelector("007");
-        User user = new User(userId, "James", "Bond", "mockdata@user.com");
-        return user;
+        this.mockUpUser = new User(userId, "James", "Bond", "mockdata@user.com");
+        return mockUpUser;
     }
     // returns mock entries
     private Entry mockEntryOne() {
         Entry.EntrySelector entryId = new Entry.EntrySelector(42);
         Category.CategorySelector categoryId = new Category.CategorySelector(10);
         Document.DocumentSelector documentId = new Document.DocumentSelector("356");
-        Entry entry = new Entry(entryId, categoryId, mockUser().getId(), documentId, "great Paper #01", "Karol Bender");
-        return entry;
+        this.mockUpEntryOne = new Entry(entryId, categoryId, this.mockUpUser.getId(), documentId, "great Paper #01", "Karol Bender");
+        return mockUpEntryOne;
     }
 
     private Entry mockEntryTwo() {
         Entry.EntrySelector entryId = new Entry.EntrySelector(420);
         Category.CategorySelector categoryId = new Category.CategorySelector(10);
         Document.DocumentSelector documentId = new Document.DocumentSelector("3560");
-        Entry entry = new Entry(entryId, categoryId, mockUser().getId(), documentId, "great Paper #02", "Karol Bender");
-        return entry;
+        this.mockUpEntryTwo = new Entry(entryId, categoryId, this.mockUpUser.getId(), documentId, "great Paper #02", "Karol Bender");
+        return mockUpEntryTwo;
     }
 
     // returns mock review
     private Review mockReview() {
         Review.ReviewSelector reviewId = new Review.ReviewSelector(mockEntryOne().getId(), 700);
-        Review review = new Review(reviewId, mockUser().getId(), mockEntryOne().getId());
-        return review;
+        this.mockUpReview = new Review(reviewId, this.mockUpUser.getId(), mockEntryOne().getId());
+        return mockUpReview;
     }
 }
