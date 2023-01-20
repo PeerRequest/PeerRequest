9002a8a55460a320cab65518c0d508bf01770f03
bits0rcerer <25325997+bits0rcerer@users.noreply.github.com>
2023-01-20 13:13:45 +0100

    Satisfy checkstyle in tests

--- src/test/java/com/peerrequest/app/PeerRequestBackendTests.java
+++ src/test/java/com/peerrequest/app/PeerRequestBackendTests.java
@@ -5,9 +5,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class PeerRequestBackendTests {
-
-	@Test
-	void contextLoads() {
-	}
-
+    @Test
+    void contextLoads() {
+    }
 }
