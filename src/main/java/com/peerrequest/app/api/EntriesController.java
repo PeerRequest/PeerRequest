d78db9af2e484f56ca311f9c2f47ee44cbeae665
Nikola <NoMail>
2023-01-22 17:34:40 +0100

    add attribute type

--- src/main/java/com/peerrequest/app/api/EntriesController.java
+++ src/main/java/com/peerrequest/app/api/EntriesController.java
@@ -55,8 +55,8 @@ public class EntriesController {
     public void deleteEntry(@PathVariable final int categoryId, @PathVariable final int entryId) {
     }
 
-    private categoryMockUp;
-    private entryMockUp;
+    private Category categoryMockUp;
+    private Entry entryMockUp;
 
     private Category mockCategory() {
         if (this.categoryMockUp != null) {
@@ -77,7 +77,7 @@ public class EntriesController {
 
     private Entry mockEntry() {
         if (this.entryMockUp != null) {
-                return entryMockUp;
+            return entryMockUp;
         }
         
         Entry.EntrySelector entryId = new Entry.EntrySelector(42);
