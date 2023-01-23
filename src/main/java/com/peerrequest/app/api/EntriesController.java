18b89e39f02b251e17848e901f7655188a94389d
veropossumus <veronikagottwald18@gmail.com>
2023-01-23 18:23:43 +0100

    update EntriesController and ReviewsController after rebase

--- src/main/java/com/peerrequest/app/api/EntriesController.java
+++ src/main/java/com/peerrequest/app/api/EntriesController.java
@@ -6,12 +6,13 @@ import com.peerrequest.app.model.Entry;
 import com.peerrequest.app.model.User;
 import org.springframework.web.bind.annotation.*;
 
-import java.time.ZonedDateTime;
 import java.util.List;
 
 @RestController
 @RequestMapping
 public class EntriesController {
+
+    private Entry entryMockUp;
 
     @GetMapping(value = "categories/{categoryId}/entries",
             produces = "application/json")
@@ -30,14 +31,14 @@ public class EntriesController {
             produces = "application/json")
     public Document getResearchPaper(@PathVariable final int categoryId,
                                      @PathVariable final int entryId) {
-        return mockEntry().getDocument();
+        return mockDocument();
     }
 
     @PostMapping(value = "categories/{categoryId}/entries",
             consumes = "application/json",
             produces = "application/json")
     public Entry postEntry(@PathVariable final int categoryId,
-                             @RequestBody final Entry entry) {
+                           @RequestBody final Entry entry) {
         this.entryMockUp = entry;
         return this.entryMockUp;
     }
@@ -46,45 +47,32 @@ public class EntriesController {
             consumes = "application/json",
             produces = "application/json")
     public Entry patchEntry(@PathVariable final int categoryId,
-                                    @PathVariable final int entryId,
-                                    @RequestBody final Entry.EntryUpdater entryUpdater) {
+                            @PathVariable final int entryId,
+                            @RequestBody final Entry.EntryUpdater entryUpdater) {
         return mockEntry();
     }
 
-    @DeleteMapping(value ="categories/{categoryId}/entries/{entryId}", consumes = "application/json")
+    @DeleteMapping(value = "categories/{categoryId}/entries/{entryId}", consumes = "application/json")
     public void deleteEntry(@PathVariable final int categoryId, @PathVariable final int entryId) {
-    }
-
-    private Category categoryMockUp;
-    private Entry entryMockUp;
-
-    private Category mockCategory() {
-        if (this.categoryMockUp != null) {
-                return categoryMockUp;
-        }
-
-        Category.CategorySelector categoryId = new Category.CategorySelector(12);
-        Category category = new Category(categoryId,
-                new User.UserSelector("140314"),
-                "Best International Conference",
-                Category.CategoryLabel.INTERNAL,
-                2020, ZonedDateTime.now(),
-                0,
-                5,
-                1);
-        return category;
     }
 
     private Entry mockEntry() {
         if (this.entryMockUp != null) {
-                return entryMockUp;
+            return entryMockUp;
         }
-        
+
         Entry.EntrySelector entryId = new Entry.EntrySelector(42);
         User.UserSelector userId = new User.UserSelector("123");
         Document.DocumentSelector documentId = new Document.DocumentSelector("356");
-        Entry entry = new Entry(entryId, mockCategory().getId(), userId, documentId, "great Paper #01",
+        Entry entry = new Entry(entryId,
+                new Category.CategorySelector(12),
+                userId, documentId, "great Paper #01",
                 "Karol Bender");
         return entry;
+    }
+
+    private Document mockDocument() {
+        Document.DocumentSelector documentId = new Document.DocumentSelector("356");
+        return new Document(documentId);
     }
 }
