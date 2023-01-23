0fcc8f210e9a886907bce26448fc9e95772c86f2
veropossumus <veronikagottwald18@gmail.com>
2023-01-23 18:23:43 +0100

    update EntriesController and ReviewsController after rebase

--- src/main/java/com/peerrequest/app/api/ReviewsController.java
+++ src/main/java/com/peerrequest/app/api/ReviewsController.java
@@ -8,6 +8,8 @@ import java.util.List;
 
 public class ReviewsController {
 
+    private Review reviewMockup;
+
     @GetMapping(value = "/categories/{categoryId}/entries/{entryId}/reviews",
             produces = "application/json")
     public List<Review> getReviews(@PathVariable final int categoryId, @PathVariable final int entryId) {
@@ -27,15 +29,15 @@ public class ReviewsController {
     public Document getReviewDocument(@PathVariable final int categoryId,
                                       @PathVariable final int entryId,
                                       @PathVariable final int reviewId) {
-        return mockReview().getReviewDocument();
+        return mockDocument();
     }
 
     @PostMapping(value = "/categories/{categoryId}/entries/{entryId}/reviews",
             consumes = "application/json",
             produces = "application/json")
     public Review postReview(@PathVariable final int categoryId,
-                               @PathVariable final int entryId,
-                               @RequestBody final Review review) {
+                             @PathVariable final int entryId,
+                             @RequestBody final Review review) {
         this.reviewMockup = review;
         return this.reviewMockup;
     }
@@ -44,69 +46,36 @@ public class ReviewsController {
             consumes = "application/json",
             produces = "application/json")
     public Document putReviewDocument(@PathVariable final int categoryId,
-                                    @PathVariable final int entryId,
-                                    @PathVariable final int reviewId,
-                                    @RequestBody final Document document) {
-        this.reviewMockup.setReviewDocument(document);
-        return this.reviewMockup.getReviewDocument();                             
+                                      @PathVariable final int entryId,
+                                      @PathVariable final int reviewId,
+                                      @RequestBody final Document document) {
+        return mockDocument();
     }
-
 
     @PatchMapping(value = "categories/{categoryId}/entries/{entryId}/reviews/{reviewId}",
             consumes = "application/json",
             produces = "application/json")
     public Review patchReview(@PathVariable final int categoryId,
-                                      @PathVariable final int entryId,
-                                      @PathVariable final int reviewId,
-                                      @RequestBody final Review.ReviewUpdater reviewUpdater) {
+                              @PathVariable final int entryId,
+                              @PathVariable final int reviewId,
+                              @RequestBody final Review.ReviewUpdater reviewUpdater) {
         return mockReview();
     }
 
-    private Category categoryMockUp;
-    private Entry entryMockUp;
-    private Review reviewMockup;
-
-    private Category mockCategory() {
-        if (this.categoryMockUp != null) {
-                return categoryMockUp;
-        }
-
-        Category.CategorySelector categoryId = new Category.CategorySelector(12);
-        Category category = new Category(categoryId,
-                new User.UserSelector("140314"),
-                "Best International Conference",
-                Category.CategoryLabel.INTERNAL,
-                2020, ZonedDateTime.now(),
-                0,
-                5,
-                1);
-        return category;
-    }
-
-    private Entry mockEntry() {
-        if (this.entryMockUp != null) {
-                return entryMockUp;
-        }
-
-        Entry.EntrySelector entryId = new Entry.EntrySelector(42);
-        User.UserSelector userId = new User.UserSelector("123");
-        Document.DocumentSelector documentId = new Document.DocumentSelector("356");
-        Entry entry = new Entry(entryId, mockCategory().getId(), userId, documentId, "great Paper #01",
-                "Karol Bender");
-        return entry;
-    }
 
     private Review mockReview() {
         if (this.reviewMockup != null) {
-                return reviewMockup;
+            return reviewMockup;
         }
 
-        Review.ReviewSelector reviewId = new Review.ReviewSelector(mockEntry().getId(), 13);
-        Review review = new Review(reviewId, new User.UserSelector("89624"), mockEntry().getId());
-        Document.DocumentSelector documentId = new Document.DocumentSelector("19126");
-        Document document = new Document(documentId);
-        review.setReviewDocument(document);
+        Review.ReviewSelector reviewId = new Review.ReviewSelector(new Entry.EntrySelector(42), 13);
+        Review review = new Review(reviewId, new User.UserSelector("89624"), new Entry.EntrySelector(42));
         return review;
+    }
+
+    private Document mockDocument() {
+        Document.DocumentSelector documentId = new Document.DocumentSelector("356");
+        return new Document(documentId);
     }
 
 }
