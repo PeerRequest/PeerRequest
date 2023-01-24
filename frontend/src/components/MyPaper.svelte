da7acbe8bd2d2377b239f546b1d2bffb3779daf7
veropossumus <veronikagottwald18@gmail.com>
2023-01-24 19:41:29 +0100

    re-add /my-papers and +page.svelte

---
+++ frontend/src/components/MyPaper.svelte
@@ -0,0 +1,14 @@
+<script>
+    import { BreadcrumbItem, TableBodyCell, TableBodyRow } from "flowbite-svelte";
+    export let myPaper;
+</script>
+
+<TableBodyRow>
+    <TableBodyCell>
+        <BreadcrumbItem href="/categories/{myPaper.category.id}/{myPaper.id}">{myPaper.title}</BreadcrumbItem>
+    </TableBodyCell>
+    <TableBodyCell>
+        <BreadcrumbItem href="/categories/{myPaper.category.id}">{myPaper.category.name}</BreadcrumbItem>
+    </TableBodyCell>
+    <!-- <TableBodyCell>{myPaper.category.year}</TableBodyCell> -->
+</TableBodyRow>
---
+++ frontend/src/components/MyPapers.svelte
@@ -0,0 +1,14 @@
+<script>
+    import { Table, TableBody, TableHead, TableHeadCell } from "flowbite-svelte";
+</script>
+
+
+<Table hoverable={true} shadow={true}>
+    <TableHead>
+        <TableHeadCell>Title</TableHeadCell>
+        <TableHeadCell>Conference</TableHeadCell>
+    </TableHead>
+    <TableBody class="divide-y">
+        <slot />
+    </TableBody>
+</Table>
