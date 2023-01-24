44788763c7ae5d0212487a81232b4efce32f998c
upxee <upxee@student.kit.edu>
2023-01-24 17:20:56 +0100

    removed ConfigurationServiceFactory + checkstyle for ServiceFactory

--- src/main/java/com/peerrequest/app/services/ServiceFactory.java
+++ src/main/java/com/peerrequest/app/services/ServiceFactory.java
@@ -7,42 +7,49 @@ public interface ServiceFactory {
 
     /**
      * Creates a new ReviewService instance.
+     *
      * @return a new ReviewService instance
      */
     ReviewService newReviewService();
 
     /**
      * Creates a new EntryService instance.
+     *
      * @return a new EntryService instance
      */
     EntryService newEntryService();
 
     /**
      * Creates a new RequestService instance.
+     *
      * @return the new RequestService instance
      */
     RequestService newRequestService();
 
     /**
      * Creates a new NotificationService instance.
+     *
      * @return the new NotificationService instance
      */
     NotificationService newNotificationService();
 
     /**
      * Creates a new DocumentService instance.
+     *
      * @return the new DocumentService instance
      */
     DocumentService newDocumentService();
 
     /**
      * Creates a new CategoryService instance.
+     *
      * @return the new CategoryService instance
      */
     CategoryService newCategoryService();
 
     /**
      * Creates a new UserService instance.
+     *
      * @return the new UserService instance
      */
     UserService newUserService();
