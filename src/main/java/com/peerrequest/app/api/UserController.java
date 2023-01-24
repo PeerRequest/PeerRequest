83ca1770cfb7f90db97db1be3ebdbadb13235adb
upxee <upxee@student.kit.edu>
2023-01-24 16:19:13 +0100

    add missing import for lList

--- src/main/java/com/peerrequest/app/api/UserController.java
+++ src/main/java/com/peerrequest/app/api/UserController.java
@@ -6,6 +6,7 @@ import com.peerrequest.app.model.Entry;
 import com.peerrequest.app.model.Review;
 import com.peerrequest.app.model.User;
 import java.util.ArrayList;
+import java.util.List;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
