364faccfbf080673a14ccea3fbfdc61de5551b67
upxee <upxee@student.kit.edu>
2023-01-24 16:16:32 +0100

    checkstyle + javadoc for UserController

--- src/main/java/com/peerrequest/app/api/UserController.java
+++ src/main/java/com/peerrequest/app/api/UserController.java
@@ -1,18 +1,33 @@
 package com.peerrequest.app.api;
 
-import com.peerrequest.app.model.*;
+import com.peerrequest.app.model.Category;
+import com.peerrequest.app.model.Document;
+import com.peerrequest.app.model.Entry;
+import com.peerrequest.app.model.Review;
+import com.peerrequest.app.model.User;
+import java.util.ArrayList;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.ArrayList;
 
-
+/**
+ * The controller class for users to encapsulate the functionalities of the services
+ *  * and provide them as HTTP-endpoints.
+ */
 @RestController
 @RequestMapping
 public class UserController {
 
+    private Entry.EntrySelector entryId;
+
+    /**
+     * Method used to return all reviews of a specific user.
+     *
+     * @param userId the id of the user
+     * @return a list with all the reviews of the user
+     */
     @GetMapping(value = "/users/{userId}/reviews", produces = "application/json")
     public List<Review> getReviews(@PathVariable int userId) {
         List<Review> reviewsList = new ArrayList<>();
@@ -20,6 +35,12 @@ public class UserController {
         return reviewsList;
     }
 
+    /**
+     * Method used to return all entries of a specific user.
+     *
+     * @param userId the id of the user
+     * @return a list with all the entries of the user
+     */
     @GetMapping(value = "/users/{userId}/entries", produces = "application/json")
     public List<Entry> getEntries(@PathVariable int userId) {
         List<Entry> entryList = new ArrayList<>();
@@ -33,6 +54,9 @@ public class UserController {
     private Entry mockUpEntryTwo;
     private Review mockUpReview;
 
+    /**
+     * Constructor for the class userController in order to initialize mock data.
+     */
     public UserController() {
         mockUser();
         mockEntryOne();
@@ -46,12 +70,14 @@ public class UserController {
         this.mockUpUser = new User(userId, "James", "Bond", "mockdata@user.com");
         return mockUpUser;
     }
+
     // returns mock entries
     private Entry mockEntryOne() {
         Entry.EntrySelector entryId = new Entry.EntrySelector(42);
         Category.CategorySelector categoryId = new Category.CategorySelector(10);
         Document.DocumentSelector documentId = new Document.DocumentSelector("356");
-        this.mockUpEntryOne = new Entry(entryId, categoryId, this.mockUpUser.getId(), documentId, "great Paper #01", "Karol Bender");
+        this.mockUpEntryOne = new Entry(entryId, categoryId, this.mockUpUser.getId(), documentId,
+            "great Paper #01", "Karol Bender");
         return mockUpEntryOne;
     }
 
@@ -59,7 +85,8 @@ public class UserController {
         Entry.EntrySelector entryId = new Entry.EntrySelector(420);
         Category.CategorySelector categoryId = new Category.CategorySelector(10);
         Document.DocumentSelector documentId = new Document.DocumentSelector("3560");
-        this.mockUpEntryTwo = new Entry(entryId, categoryId, this.mockUpUser.getId(), documentId, "great Paper #02", "Karol Bender");
+        this.mockUpEntryTwo = new Entry(entryId, categoryId, this.mockUpUser.getId(), documentId,
+            "great Paper #02", "Karol Bender");
         return mockUpEntryTwo;
     }
 
