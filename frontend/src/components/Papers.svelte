08e63822e9369b5b5684bf8a0acb8292ee28303d
veropossumus <veronikagottwald18@gmail.com>
2023-01-26 15:46:00 +0100

    combine MyPaper(s) and Paper(s) to avoid duplicate code

--- frontend/src/components/Papers.svelte
+++ frontend/src/components/Papers.svelte
@@ -1,19 +1,26 @@
 <script>
-  import { Table, TableBody, TableHead, TableHeadCell } from "flowbite-svelte";
+    import {Table, TableBody, TableHead, TableHeadCell} from "flowbite-svelte";
 
-  export let category_type;
+    export let category_type;
+    export let title;
+    export let show_category;
 </script>
 
 <Table noborder={false} shadow={true}>
-  <TableHead>
-    <TableHeadCell>Title</TableHeadCell>
+    <TableHead>
+        <TableHeadCell>Title</TableHeadCell>
 
-    {#if category_type === "Internal" }
-      <TableHeadCell>Open Review Slots</TableHeadCell>
-      <TableHeadCell></TableHeadCell>
-    {/if}
-  </TableHead>
-  <TableBody class="divide-y">
-    <slot></slot>
-  </TableBody>
+        {#if show_category}
+            <TableHeadCell>Conference</TableHeadCell>
+        {/if}
+
+        {#if (category_type != null) && (category_type === "Internal") }
+            <TableHeadCell>Open Review Slots</TableHeadCell>
+            <TableHeadCell></TableHeadCell>
+        {/if}
+
+    </TableHead>
+    <TableBody class="divide-y">
+        <slot></slot>
+    </TableBody>
 </Table>
