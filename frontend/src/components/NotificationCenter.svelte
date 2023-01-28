8913324fd2afc524b9045c0bd6cdf0d60aace463
upxee <upxee@student.kit.edu>
2023-01-28 14:44:03 +0100

    tailwind css instead of regular css

--- frontend/src/components/NotificationCenter.svelte
+++ frontend/src/components/NotificationCenter.svelte
@@ -6,24 +6,7 @@
     let show = false;
 </script>
 
-<style>
-    /* unread message count */
-    .badge {
-        display: inline-block;
-        position: absolute;
-        top: 0;
-        background-color: #4285f4;
-        color: #d7e6fd;
-        right: 0;
-        border-radius: 9999px;
-        font-size: 12px;
-        min-width: 18px;
-        line-height: 18px;
-        min-height: 18px;
-        text-align: center;
-    }
-</style>
-
+<!-- notification bell -->
 <button class="relative p-1" on:click={() => (show = !show)}>
     <svg
             xmlns="http://www.w3.org/2000/svg"
@@ -40,7 +23,7 @@
         <path d="M18 8A6 6 0 0 0 6 8c0 7-3 9-3 9h18s-3-2-3-9" />
         <path d="M13.73 21a2 2 0 0 1-3.46 0" />
     </svg>
-    <span class="badge">{notifications.length}</span>
+    <span class="inline-block absolute top-0 bg-blue-500 text-white right-0 rounded-full text-xs min-w-[18px] min-h-[18px] text-center">{notifications.length}</span>
 </button>
 
 {#if show}
