3b72ad99e5e91f3d04913a96665cf0733cb09143
upxee <upxee@student.kit.edu>
2023-01-29 16:15:28 +0100

    different view for submitting internal/external Papers now in SubmitPaper.svelte

--- frontend/src/components/SubmitPaperModal.svelte
+++ frontend/src/components/SubmitPaperModal.svelte
@@ -34,23 +34,18 @@
 </script>
 
 <Modal bind:open={show} on:hide={() => hide ? hide() : null} title="Submit New Paper" size="lg">
-    {#if (conference_type === "Internal")}
-        <form>
-            <div class="grid gap-y-6">
-                {#each papers as p}
-                    <SubmitPaper/>
-                    <hr class="my-8 h-px bg-gray-200 border-0 dark:bg-gray-700">
-                {/each}
-                <Button on:click={() => addPaper()}> Add Additional Paper</Button>
-                {#if (papers.length !== 0)}
-                    <Button pill class="!p-2" outline color="red"
-                            on:click={() => papers = papers.filter(e => e !== papers[papers.length -1])}>Remove Last Paper</Button>
-                {/if}
-                <Button type="submit" color="primary" size="xs" on:click={() => finishSubmission()}>Finish Submission</Button>
-            </div>
-        </form>
-    {/if}
-    {#if (conference_type === "External")}
-        <PdfUploader/>
-    {/if}
+    <form>
+        <div class="grid gap-y-6">
+            {#each papers as p}
+                <SubmitPaper category_type={conference_type}/>
+                <hr class="my-8 h-px bg-gray-200 border-0 dark:bg-gray-700">
+            {/each}
+            <Button on:click={() => addPaper()}> Add Additional Paper</Button>
+            {#if (papers.length !== 0)}
+                <Button pill class="!p-2" outline color="red"
+                        on:click={() => papers = papers.filter(e => e !== papers[papers.length -1])}>Remove Last Paper</Button>
+            {/if}
+            <Button type="submit" color="primary" size="xs" on:click={() => finishSubmission()}>Finish Submission</Button>
+        </div>
+    </form>
 </Modal>
