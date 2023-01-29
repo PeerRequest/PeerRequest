f2194cbfcd023dbc6ceb5549c6222ed0e0bddb3e
bits0rcerer <25325997+bits0rcerer@users.noreply.github.com>
2023-01-29 17:09:27 +0100

    implement account management in frontend

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -2,7 +2,14 @@ import {sveltekit} from '@sveltejs/kit/vite';
 import type {UserConfig} from 'vite';
 
 const config: UserConfig = {
-    plugins: [sveltekit()]
+    plugins: [sveltekit()],
+    server: {
+        proxy: {
+            '/api': 'http://localhost:8080',
+            '/dev-login': 'http://localhost:8080',
+            '/logout': 'http://localhost:8080',
+        },
+    },
 };
 
 export default config;
