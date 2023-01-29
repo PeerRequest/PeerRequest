37475101bfe7da1dd4e29d14128d7c75798086d1
veropossumus <veronikagottwald18@gmail.com>
2023-01-29 15:42:54 +0100

    rebase onto master

---
+++ frontend/src/components/Biddings.svelte
@@ -0,0 +1,13 @@
+<script>
+    import {Table, TableBody, TableHead, TableHeadCell} from "flowbite-svelte";
+</script>
+
+<Table noborder={false} shadow={true}>
+    <TableHead>
+        <TableHeadCell>Title</TableHeadCell>
+        <TableHeadCell>Rating</TableHeadCell>
+    </TableHead>
+    <TableBody class="divide-y">
+        <slot></slot>
+    </TableBody>
+</Table>
