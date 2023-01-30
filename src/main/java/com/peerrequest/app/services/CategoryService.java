16c13932c9f9b53d3a754bde90b2efa0ac49d745
Nikola <ulsxl@student.kit.edu>
2023-01-30 19:51:03 +0100

    add javadoc

--- src/main/java/com/peerrequest/app/services/CategoryService.java
+++ src/main/java/com/peerrequest/app/services/CategoryService.java
@@ -4,6 +4,9 @@ import com.peerrequest.app.model.Category;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+/**
+ * Category JPA Repository, using Long as ID and Category as relation.
+ */
 @Repository("CategoryService")
 public interface CategoryService extends JpaRepository<Category, Long> {
 }
