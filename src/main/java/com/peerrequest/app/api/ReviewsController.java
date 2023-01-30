635f74980abbeaca8cb555eb487d87c6d83dcdcd
Wil-Lee <mildt.michael@gmail.com>
2023-01-30 19:43:05 +0100

    fix annotations and wrong path variable

--- src/main/java/com/peerrequest/app/api/ReviewsController.java
+++ src/main/java/com/peerrequest/app/api/ReviewsController.java
@@ -6,17 +6,14 @@ import com.peerrequest.app.model.Review;
 import com.peerrequest.app.model.User;
 import java.util.List;
 import java.util.Objects;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PatchMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.*;
 
 
 /**
  * Defines the controller for Reviews.
  */
+@RestController
+@ApiControllerPrefix
 public class ReviewsController {
 
     private Review reviewMockup;
