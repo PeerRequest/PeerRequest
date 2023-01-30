2a5ffbe515c59f611e8d8621d4075282baf85465
Nikola <ulsxl@student.kit.edu>
2023-01-30 19:07:01 +0100

    remove mockup, add dummy implementation

--- src/main/java/com/peerrequest/app/api/CategoriesController.java
+++ src/main/java/com/peerrequest/app/api/CategoriesController.java
@@ -1,12 +1,9 @@
 package com.peerrequest.app.api;
 
 import com.peerrequest.app.model.Category;
-import com.peerrequest.app.model.Document;
-import com.peerrequest.app.model.Entry;
-import com.peerrequest.app.model.Review;
-import com.peerrequest.app.model.User;
-import java.time.ZonedDateTime;
 import java.util.List;
+
+import com.peerrequest.app.services.CategoryService;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
@@ -23,79 +20,36 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @ApiControllerPrefix
 public class CategoriesController {
-    private Category mockUpCategory;
 
-    public CategoriesController() {
-        mockCategory();
+    private final CategoryService categoryService;
+
+    public CategoriesController(CategoryService categoryService) {
+        this.categoryService = categoryService;
     }
 
     @GetMapping(value = "/categories", produces = "application/json")
     public List<Category> getCategories() {
-        return List.of(mockUpCategory);
+        return categoryService.findAll();
     }
 
     @GetMapping(value = "/categories/{categoryId}", produces = "application/json")
-    public Category getCategory(@PathVariable final int categoryId) {
-        return mockUpCategory;
+    public Category getCategory(@PathVariable final Long categoryId) {
+        return categoryService.getReferenceById(categoryId);
     }
 
     @PostMapping(value = "/categories", produces = "application/json")
     public Category createCategory(@RequestBody final Category category) {
-        this.mockUpCategory = category;
-        return mockUpCategory;
+        return categoryService.saveAndFlush(category);
     }
 
     @PatchMapping(value = "/categories/{categoryId}", consumes = "application/json", produces = "application/json")
     public Category patchCategory(@RequestBody final Category category) {
-        return mockUpCategory;
+        throw new RuntimeException("Not Implemented");
     }
 
     @DeleteMapping(value = "categories/{categoryId}/", consumes = "application/json")
     public void deleteCategory(@PathVariable final int categoryId) {
+        throw new RuntimeException("Not Implemented");
     }
 
-    // returns mock category
-    private Category mockCategory() {
-        Category.CategorySelector categoryId = new Category.CategorySelector(12);
-        this.mockUpCategory = new Category(categoryId,
-            new User.UserSelector("140314"),
-            "Best International Conference",
-            Category.CategoryLabel.INTERNAL,
-            2020, ZonedDateTime.now(),
-            0,
-            5,
-            1);
-        return mockUpCategory;
-    }
-
-    //returns mock user
-    private User mockUser() {
-        User.UserSelector userId = new User.UserSelector("007");
-        User user = new User(userId, "James", "Bond", "mockdata@user.com");
-        return user;
-    }
-
-    // returns mock entries
-    private Entry mockEntryOne() {
-        Entry.EntrySelector entryId = new Entry.EntrySelector(42);
-        Document.DocumentSelector documentId = new Document.DocumentSelector("356");
-        Entry entry = new Entry(entryId, mockCategory().getId(), mockUser().id(), documentId,
-            "great Paper #01", "Karol Bender");
-        return entry;
-    }
-
-    private Entry mockEntryTwo() {
-        Entry.EntrySelector entryId = new Entry.EntrySelector(420);
-        Document.DocumentSelector documentId = new Document.DocumentSelector("3560");
-        Entry entry = new Entry(entryId, mockCategory().getId(), mockUser().id(), documentId,
-            "great Paper #02", "Karol Bender");
-        return entry;
-    }
-
-    // returns mock review
-    private Review mockReview() {
-        Review.ReviewSelector reviewId = new Review.ReviewSelector(mockEntryOne().getId(), 700);
-        Review review = new Review(reviewId, mockUser().id(), mockEntryOne().getId());
-        return review;
-    }
 }
