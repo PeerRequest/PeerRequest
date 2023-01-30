7e7b24f2ab6e9bb69b1b9b5b29af8606d164ef96
Nikola <ulsxl@student.kit.edu>
2023-01-30 18:38:52 +0100

    CategoryService now extends JpaRepository

--- src/main/java/com/peerrequest/app/services/CategoryService.java
+++ src/main/java/com/peerrequest/app/services/CategoryService.java
@@ -1,13 +1,9 @@
 package com.peerrequest.app.services;
 
 import com.peerrequest.app.model.Category;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
-/**
- * Describes the functionality of a CategoryService.
- *
- * @param <F> Category Filter
- * @param <U> Category Updater
- */
-public interface CategoryService<F, U>
-    extends CrudService<Category, Category.CategorySelector, F, U> {
+@Repository("CategoryService")
+public interface CategoryService extends JpaRepository<Category, Long> {
 }
