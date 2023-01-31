1fdf03c9409be2c3f38374e008bfcf4c4a01ea35
Nikola <ulsxl@student.kit.edu>
2023-01-31 18:25:30 +0100

    add EntryService Interface

---
+++ src/main/java/com/peerrequest/app/services/EntryService.java
@@ -0,0 +1,10 @@
+package com.peerrequest.app.services;
+
+import com.peerrequest.app.data.Entry;
+
+/**
+ * Describes the functionality of an EntryService.
+ */
+public interface EntryService
+    extends CrudService<Entry, Long, Entry.Dto> {
+}
