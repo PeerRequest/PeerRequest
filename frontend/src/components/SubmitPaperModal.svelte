bf3d896e18188bd823ad475ee8799ab695489c24
upxee <upxee@student.kit.edu>
2023-02-01 13:47:00 +0100

    submit-button now inside form component

--- frontend/src/components/SubmitPaperModal.svelte
+++ frontend/src/components/SubmitPaperModal.svelte
@@ -2,7 +2,8 @@
     import {
         Modal,
         Button,
-        CloseButton
+        CloseButton,
+        Footer
     } from "flowbite-svelte" ;
     import SubmitPaper from "./SubmitPaper.svelte";
 
@@ -32,7 +33,7 @@
 
 </script>
 
-<Modal bind:open={show} on:hide={() => hide ? hide() : null} size="lg" permanent = {true}>
+<Modal bind:open={show} on:hide={() => hide ? hide() : null} size="lg" permanent = {true} >
     <svelte:fragment slot="header">
         <div class="text-4xl font-extrabold text-gray-900">
             Submit New Paper
@@ -40,22 +41,22 @@
         <CloseButton class="absolute top-3 right-5"
                      on:click={hide}/>
     </svelte:fragment>
-    <form>
+    <form class="min-h-[20vh]">
         <div class="grid grid-cols-1 col-span-full gap-y-6 flex justify-center">
             {#each papers as p}
                 <SubmitPaper category_type={conference_type}/>
                 <hr class="my-8 h-px bg-gray-200 border-0 dark:bg-gray-700">
             {/each}
         </div>
+        <Footer class="absolute bottom-0 left-0 z-20 w-full">
+            <div class="grid grid-cols-1 col-span-full gap-y-6 flex justify-center w-full ">
+                <Button outline on:click={() => addPaper()}> Add Additional Paper</Button>
+                {#if (papers.length !== 0)}
+                    <Button class="!p-2" outline color="red"
+                            on:click={() => papers = papers.filter(e => e !== papers[papers.length -1])}>Remove Last Paper</Button>
+                {/if}
+                <Button class="w-full" type="submit" color="primary" size="xs" on:click={() => finishSubmission()}>Finish Submission</Button>
+            </div>
+        </Footer>
     </form>
-    <svelte:fragment slot="footer">
-        <div class="grid grid-cols-1 col-span-full gap-y-6 flex justify-center w-full ">
-            <Button outline on:click={() => addPaper()}> Add Additional Paper</Button>
-            {#if (papers.length !== 0)}
-                <Button class="!p-2" outline color="red"
-                        on:click={() => papers = papers.filter(e => e !== papers[papers.length -1])}>Remove Last Paper</Button>
-            {/if}
-            <Button class="w-full" type="submit" color="primary" size="xs" on:click={() => finishSubmission()}>Finish Submission</Button>
-        </div>
-    </svelte:fragment>
 </Modal>
