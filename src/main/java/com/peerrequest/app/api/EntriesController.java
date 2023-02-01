8f0d2a07e9036405ba95ac5172a7ae23f221e2de
Nikola <ulsxl@student.kit.edu>
2023-02-01 18:02:52 +0100

    implement EntriesController

--- src/main/java/com/peerrequest/app/api/EntriesController.java
+++ src/main/java/com/peerrequest/app/api/EntriesController.java
@@ -9,8 +9,6 @@ import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
 
-
-
 /**
  * The controller class for entries to encapsulate the functionalities of the corresponding
  * service and provide them as HTTP-endpoints.
@@ -22,21 +20,31 @@ public class EntriesController extends ServiceBasedController {
     private final int maxPageSize = 100;
 
     @GetMapping("/categories/{category_id}/entries")
-    List<Entry.Dto> listEntries(@RequestParam Optional<Integer> limit, @RequestParam Optional<Long> after) {
+    List<Entry.Dto> listEntries(@RequestParam Optional<Integer> limit,
+                                @RequestParam Optional<Long> after,
+                                @PathVariable Long category_id) {
         if (limit.isPresent()) {
             if (limit.get() < 0) {
                 throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "limit must be greater than 0");
             }
             limit = Optional.of(Math.min(limit.get(), maxPageSize));
         }
-        // TODO: Implement after EntryService is finished
-        throw new RuntimeException("Not implemented yet");
+
+        Entry.Dto filterEntry = new Entry.Dto(
+                null, null, null, null, Optional.of(category_id));
+
+        return this.entryService.list(after.orElse(null), limit.orElse(maxPageSize), filterEntry)
+                .stream().map(Entry::toDto).toList();
     }
 
     @GetMapping("/categories/{category_id}/entries/{entry_id}")
     Entry.Dto getEntry(@PathVariable(name = "entry_id") Long id) {
-        // TODO: Implement after EntryService is finished
-        throw new RuntimeException("Not implemented yet");
+        var option = this.entryService.get(id);
+        if (option.isEmpty()) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "entry does not exist");
+        }
+
+        return option.get().toDto();
     }
 
     @GetMapping("/categories/{category_id}/entries/{entry_id}/paper")
@@ -46,15 +54,45 @@ public class EntriesController extends ServiceBasedController {
     }
 
     @PostMapping("/categories/{category_id}/entries")
-    Entry.Dto createEntries(@RequestBody List<Entry.Dto> dto,
-                                  @AuthenticationPrincipal OAuth2User user) {
-        // TODO: Implement after EntryService is finished
-        throw new RuntimeException("Not implemented yet");
+    Entry.Dto createEntries(@RequestBody Entry.Dto dto,
+                            @AuthenticationPrincipal OAuth2User user, @PathVariable Long category_id) {
+        if (dto.name() == null) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "name is required");
+        }
+
+        // TODO: Only allow entry creation if category exists AND user is authorized
+
+        if (dto.documentId() == null) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "document_id is required");
+        }
+
+        var entry = Entry.fromDto(dto, user.getAttribute("sub"), category_id);
+        return this.entryService.create(entry.toDto()).toDto();
     }
 
-    @PatchMapping("/categories/{category_id}/entries/{entry_id}")
-    Entry.Dto patchEntries (@RequestBody Entry.Dto dto) {
-        // TODO: Implement after EntryService is finished
-        throw new RuntimeException("Not implemented yet");
+    @PatchMapping("/categories/{category_id}/entries")
+    Entry.Dto patchEntries(@RequestBody Entry.Dto dto, @AuthenticationPrincipal OAuth2User user) {
+        if (dto.id().isEmpty()) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "id is required");
+        }
+
+        if (dto.researcherId().isPresent()) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "you may not change the researcher_id");
+        }
+
+        var patchEntry = this.entryService.get(dto.id().get());
+
+        if (patchEntry.isEmpty()) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "entry does not exist");
+        }
+
+        if (!patchEntry.get().getResearcherId().equals(user.getAttribute("sub"))) {
+            throw new ResponseStatusException(HttpStatus.FORBIDDEN,
+                    "Only the user that created the entry may patch it");
+        }
+
+        var option = this.entryService.update(dto.id().get(), dto);
+
+        return option.get().toDto();
     }
 }
