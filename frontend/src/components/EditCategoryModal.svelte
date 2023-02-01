25a5757dd9535a7b30f685ea5896eb103d110325
upxee <upxee@student.kit.edu>
2023-02-01 18:10:30 +0100

    fixed sizes and rounded input-boxes

--- frontend/src/components/EditCategoryModal.svelte
+++ frontend/src/components/EditCategoryModal.svelte
@@ -30,16 +30,16 @@
     <form class="grid gap-y-6">
         <div class="flex flex-row justify-between items-center">
             <Heading size="md" tag="h4"> Name </Heading>
-            <input class="min-w-[13.5rem]" id= conference_name type= text value={conference.name} size="{conference.name.length}"
+            <input class="min-w-[13.5rem] w-full rounded-lg" id= conference_name type= text value={conference.name} size="{conference.name.length}"
                    onkeypress="this.style.width = ((this.value.length + 8) * 8) + 'px';" required>
         </div>
         <div class="flex flex-row justify-between items-center">
             <Heading size="md" tag="h4"> Year </Heading>
-            <input id= conference_year type= number value={conference.year} required>
+            <input class="w-full rounded-lg" id= conference_year type= number value={conference.year} required>
         </div>
         <div class="flex flex-row justify-between items-center">
             <Heading size="md" tag="h4"> Deadline </Heading>
-            <input id= conference_deadline type= date value={conference.deadline} required>
+            <input class="w-full rounded-lg" id= conference_deadline type= date value={conference.deadline} required>
         </div>
         <Button type="submit" color="primary" size="xs">
             Save
