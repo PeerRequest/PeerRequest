57f854a200d4c93b9fefc0388b01d3bbe2d02fbc
upxee <upxee@student.kit.edu>
2023-02-02 15:53:04 +0100

    remove stores

--- frontend/src/mock_data.js
+++ frontend/src/mock_data.js
@@ -135,10 +135,21 @@ const reviews = [
         paper: papers[1]
     }
 ]
+
+const comments = [
+    {
+        id: 0,
+        user: "Mysterious other user",
+        content: "Lorem ipsum dolor sit amet, adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+        date: "Mon Apr 12 2021 14:41:24 GMT-0400 (Horário Padrão do Amazonas)"
+    }
+]
+
 export default {
     pagination,
     categories,
     papers,
     users,
-    reviews
+    reviews,
+    comments
 };
