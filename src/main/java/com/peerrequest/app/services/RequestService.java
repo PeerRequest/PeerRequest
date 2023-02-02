ec89de9a0280013b9b4661661c75a603308ac7fe
Nikola <ulsxl@student.kit.edu>
2023-02-02 13:12:01 +0100

    add Request Service

---
+++ src/main/java/com/peerrequest/app/services/RequestService.java
@@ -0,0 +1,11 @@
+package com.peerrequest.app.services;
+
+import com.peerrequest.app.data.Request;
+
+/**
+ * Describes the functionality of a RequestService.
+ */
+public interface RequestService
+        extends CrudService<Request, Long, Request.Dto> {
+}
+
