a097e1f05507b6c6f79bc9429eb8cf4ad42ede3e
Nikola <ulsxl@student.kit.edu>
2023-02-02 16:25:03 +0100

    add dummy DirectRequest repo

---
+++ src/main/java/com/peerrequest/app/data/repos/DirectRequestRepository.java
@@ -0,0 +1,33 @@
+package com.peerrequest.app.data.repos;
+
+import com.peerrequest.app.data.DirectRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
+
+import java.util.List;
+
+public interface DirectRequestRepository extends CrudRepository<DirectRequest, Long> {
+    /**
+     * Defines the list operation.
+     *
+     * @param cursor   if not null the last id of the previous page
+     * @param pageable describes the pages
+     * @return a list of all matching entries
+     */
+    default List<DirectRequest> list(Long cursor, Pageable pageable, Long directRequestProcessId) {
+
+        if (cursor == null) {
+            return list(pageable);
+        } else {
+            return list(cursor, pageable);
+        }
+    }
+
+    @Query("select e from DirectRequest e order by e.id DESC")
+    List<DirectRequest> list(Pageable pageable);
+
+    @Query("select e from DirectRequest e where e.id < ?1 order by e.id DESC")
+    List<DirectRequest> list(Long cursor, Pageable pageable);
+
+}
