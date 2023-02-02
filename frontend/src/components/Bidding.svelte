fadd0c0ac5cb632988a6080dbdda142a72fdddf6
veropossumus <16821784+veropossumus@users.noreply.github.com>
2023-02-02 19:40:43 +0100

    rating stars now let you change rating on click

--- frontend/src/components/Bidding.svelte
+++ frontend/src/components/Bidding.svelte
@@ -1,11 +1,9 @@
 <script>
     import {BreadcrumbItem, TableBodyCell, TableBodyRow} from "flowbite-svelte";
-    //import StarRating from 'svelte-star-rating';
     import StarRating from "./StarRating.svelte";
 
     export let href;
     export let title;
-
 </script>
 
 
@@ -17,3 +15,4 @@
         <StarRating />
     </TableBodyCell>
 </TableBodyRow>
+
