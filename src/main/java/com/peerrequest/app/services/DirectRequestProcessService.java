07080b2eb9f02946f914ae36bfe8cd968da912ac
Nikola <ulsxl@student.kit.edu>
2023-02-02 16:21:22 +0100

    add DirectRequest-related Service Interfaces

---
+++ src/main/java/com/peerrequest/app/services/DirectRequestProcessService.java
@@ -0,0 +1,7 @@
+package com.peerrequest.app.services;
+
+import com.peerrequest.app.data.DirectRequestProcess;
+
+public interface DirectRequestProcessService
+        extends CrudService<DirectRequestProcess, Long, DirectRequestProcess.Dto> {
+}
