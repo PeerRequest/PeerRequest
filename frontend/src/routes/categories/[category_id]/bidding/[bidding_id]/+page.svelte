8a076da8a38417995fa0954e3ae77b436a51c7b8
veropossumus <16821784+veropossumus@users.noreply.github.com>
2023-02-03 18:01:59 +0100

    add start and results button for bidding assignment

--- frontend/src/routes/categories/[category_id]/bidding/[bidding_id]/+page.svelte
+++ frontend/src/routes/categories/[category_id]/bidding/[bidding_id]/+page.svelte
@@ -1,5 +1,5 @@
 <script>
-    import {BreadcrumbItem, ChevronLeft, ChevronRight, Heading, Pagination, Secondary} from "flowbite-svelte";
+    import {BreadcrumbItem, Button, ChevronLeft, ChevronRight, Heading, Pagination, Secondary} from "flowbite-svelte";
     import mock_data from "../../../../../mock_data.js";
     import Container from "../../../../../components/Container.svelte";
     import ResponsiveBreadCrumb from "../../../../../components/ResponsiveBreadCrumb.svelte";
@@ -13,7 +13,8 @@
     const categories = mock_data.categories;
     const papers = mock_data.papers;
 
-    let rating=0;
+    let rating = 0;
+    let biddingOngoing = true;
 
     /** @type {import("./$types").PageData} */
     export let data;
@@ -25,6 +26,10 @@
     const next = () => {
         alert("Next btn clicked. Make a call to your server to fetch data.");
     };
+
+    function handleClick() {
+        biddingOngoing = !biddingOngoing;
+    }
 </script>
 
 <svelte:head>
@@ -32,18 +37,35 @@
 </svelte:head>
 
 <Container>
-    <ResponsiveBreadCrumb>
-        <BreadcrumbItem home href="/">Home</BreadcrumbItem>
-        <BreadcrumbItem href="/categories">Conferences</BreadcrumbItem>
-        <BreadcrumbItem
-                href="/categories/{categories[data.category_id - 1].id}">{categories[data.category_id - 1].name}</BreadcrumbItem>
-        <BreadcrumbItem>Bidding</BreadcrumbItem>
+    <div class="flex max-md:flex-wrap justify-between items-center mb-4">
+        <div>
+            <ResponsiveBreadCrumb>
+                <BreadcrumbItem home href="/">Home</BreadcrumbItem>
+                <BreadcrumbItem href="/categories">Conferences</BreadcrumbItem>
+                <BreadcrumbItem
+                        href="/categories/{categories[data.category_id - 1].id}">{categories[data.category_id - 1].name}</BreadcrumbItem>
+                <BreadcrumbItem>Bidding</BreadcrumbItem>
 
-    </ResponsiveBreadCrumb>
-    <Heading tag="h2">{"Bidding for " + categories[data.category_id - 1].name}</Heading>
-    <Heading class="mb-5" tag="h6">
-        <Secondary>{"Review Deadline: " + categories[data.category_id - 1].deadline }</Secondary>
-    </Heading>
+            </ResponsiveBreadCrumb>
+            <Heading tag="h2">{"Bidding for " + categories[data.category_id - 1].name}</Heading>
+            <Heading class="mb-5" tag="h6">
+                <Secondary>{"Review Deadline: " + categories[data.category_id - 1].deadline }</Secondary>
+            </Heading>
+        </div>
+
+        <div class="mr-5">
+            <Button on:click={handleClick}>
+                {biddingOngoing ? "Start bidding" : "Stop bidding"}
+            </Button>
+
+            <Button disabled={!biddingOngoing}
+                    href="./{bidding[data.bidding_id - 1].id}/assignment"
+                    outline>
+                Get results
+            </Button>
+        </div>
+
+    </div>
 
     <Papers show_rating=true>
         {#each papers as p}
