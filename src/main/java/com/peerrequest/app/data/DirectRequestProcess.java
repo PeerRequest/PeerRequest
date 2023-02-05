68cf4bb7b3bd08bf509edf53a4b7b0abc6a46f3c
Nikola <ulsxl@student.kit.edu>
2023-02-05 15:37:49 +0100

    implement patchDirectRequest endpoint

--- src/main/java/com/peerrequest/app/data/DirectRequestProcess.java
+++ src/main/java/com/peerrequest/app/data/DirectRequestProcess.java
@@ -24,7 +24,7 @@ public class DirectRequestProcess {
     @Column(name = "id", nullable = false)
     @Getter
     private Long id;
-    @Column(name = "entry_id", nullable = false)
+    @Column(name = "entry_id", nullable = false, updatable = false)
     @Getter
     private Long entryId;
     @Column(name = "open_slots", nullable = false)
