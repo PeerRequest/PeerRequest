c6db7376495ffdd78ba9870bbc27a8345231e196
upxee <upxee@student.kit.edu>
2023-02-06 21:18:21 +0100

    rebase onto master

--- frontend/src/routes/categories/+page.svelte
+++ frontend/src/routes/categories/+page.svelte
@@ -1,4 +1,5 @@
 <script>
+
     import { BreadcrumbItem, Button, ChevronLeft, ChevronRight, Heading, Pagination } from "flowbite-svelte";
     import mock_data from "../../mock_data.js";
     import Container from "../../components/Container.svelte";
@@ -28,12 +29,12 @@
         <BreadcrumbItem href="/categories">Conferences</BreadcrumbItem>
     </ResponsiveBreadCrumb>
     <Heading class="mb-4" tag="h2">Conferences</Heading>
-    <Button size="lg" class="mx-auto h-8 mb-4" outline
+    <Button size="lg" color="primary" class="mb-4"
             on:click={() => show_create_category_modal = true}>Create new Conference
     </Button>
     <Categories>
         {#each mock_data.categories as c }
-            <Category bind:category={c} />
+            <Category bind:category={c}/>
         {/each}
     </Categories>
 
@@ -41,11 +42,11 @@
         <Pagination icon on:next={next} on:previous={previous} {pages}>
             <svelte:fragment slot="prev">
                 <span class="sr-only">Previous</span>
-                <ChevronLeft class="w-5 h-5" />
+                <ChevronLeft class="w-5 h-5"/>
             </svelte:fragment>
             <svelte:fragment slot="next">
                 <span class="sr-only">Next</span>
-                <ChevronRight class="w-5 h-5" />
+                <ChevronRight class="w-5 h-5"/>
             </svelte:fragment>
         </Pagination>
     </div>
