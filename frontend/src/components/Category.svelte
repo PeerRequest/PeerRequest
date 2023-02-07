1a3ab1ed99276e23d670d13f32bdb256941bcf86
veropossumus <16821784+veropossumus@users.noreply.github.com>
2023-02-07 19:57:35 +0100

    update category components

--- frontend/src/components/Category.svelte
+++ frontend/src/components/Category.svelte
@@ -1,17 +1,40 @@
 <script>
   import { BreadcrumbItem, Button, TableBodyCell, TableBodyRow } from "flowbite-svelte";
+  import Skeleton from "svelte-skeleton-loader"
 
-  export let category;
+  export let category = null;
+  export let loading = false;
+  function map_type(type) {
+    switch (type) {
+      case "INTERNAL":
+        return "Internal";
+      case "EXTERNAL":
+        return "External";
+    }
+  }
+  function map_deadline(deadline) {
+    return new Date(Date.parse(deadline)).toLocaleString();
+  }
 </script>
 
 <TableBodyRow>
-  <TableBodyCell>
-    <BreadcrumbItem href="/categories/{category.id}">{category.name}</BreadcrumbItem>
-  </TableBodyCell>
-  <TableBodyCell>{category.type}</TableBodyCell>
-  <TableBodyCell>{category.year}</TableBodyCell>
-  <TableBodyCell>{category.deadline}</TableBodyCell>
-  <TableBodyCell>
-    <Button href="/categories/{category.id}" outline size="xs">Papers</Button>
-  </TableBodyCell>
+  {#if loading}
+    {#each [...Array(5).keys()] as i}
+      <TableBodyCell>
+        <div>
+          <Skeleton/>
+        </div>
+      </TableBodyCell>
+    {/each}
+  {:else }
+    <TableBodyCell>
+      <BreadcrumbItem href="/categories/{category.id}">{category.name}</BreadcrumbItem>
+    </TableBodyCell>
+    <TableBodyCell>{map_type(category.label)}</TableBodyCell>
+    <TableBodyCell>{category.year}</TableBodyCell>
+    <TableBodyCell>{map_deadline(category.deadline)}</TableBodyCell>
+    <TableBodyCell>
+      <Button href="/categories/{category.id}" outline size="xs">Papers</Button>
+    </TableBodyCell>
+  {/if}
 </TableBodyRow>
