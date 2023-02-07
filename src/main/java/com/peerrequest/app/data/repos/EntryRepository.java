5c487de2ee56035f0dd135f84b02b014842279b3
Nikola <ulsxl@student.kit.edu>
2023-02-07 15:34:30 +0100

    remove unnecessary imports

--- src/main/java/com/peerrequest/app/data/repos/EntryRepository.java
+++ src/main/java/com/peerrequest/app/data/repos/EntryRepository.java
@@ -5,9 +5,7 @@ import com.peerrequest.app.data.Entry;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
-import org.springframework.data.repository.PagingAndSortingRepository;
 
 
 /**
