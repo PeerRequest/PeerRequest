5e699c4499ac4f853cb2de21af7fa6af705e5eb4
veropossumus <16821784+veropossumus@users.noreply.github.com>
2023-02-07 19:57:35 +0100

    update category components

--- frontend/src/components/Categories.svelte
+++ frontend/src/components/Categories.svelte
@@ -1,18 +1,18 @@
 <script>
-  import { Table, TableBody, TableHead, TableHeadCell } from "flowbite-svelte";
+    import {Table, TableBody, TableHead, TableHeadCell} from "flowbite-svelte";
 </script>
 
 <Table hoverable={true} shadow={true}>
-  <TableHead>
-    <TableHeadCell>Conferences</TableHeadCell>
-    <TableHeadCell>Type</TableHeadCell>
-    <TableHeadCell>Year</TableHeadCell>
-    <TableHeadCell>Review Deadline</TableHeadCell>
-    <TableHeadCell>
-      <span class="sr-only"> Edit </span>
-    </TableHeadCell>
-  </TableHead>
-  <TableBody class="divide-y">
-    <slot />
-  </TableBody>
+    <TableHead>
+        <TableHeadCell>Conferences</TableHeadCell>
+        <TableHeadCell>Type</TableHeadCell>
+        <TableHeadCell>Year</TableHeadCell>
+        <TableHeadCell>Review Deadline</TableHeadCell>
+        <TableHeadCell>
+            <span class="sr-only"> Edit </span>
+        </TableHeadCell>
+    </TableHead>
+    <TableBody class="divide-y">
+        <slot/>
+    </TableBody>
 </Table>
