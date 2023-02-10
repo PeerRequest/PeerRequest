2d0367bef568c54abe5fee28e8c52f801050a59d
veropossumus <16821784+veropossumus@users.noreply.github.com>
2023-02-10 20:20:05 +0100

    send direct requests to all requested reviewers

--- frontend/src/components/CreateEntryModal.svelte
+++ frontend/src/components/CreateEntryModal.svelte
@@ -12,37 +12,12 @@
         TableBodyCell,
         Chevron,
         Dropdown,
-        Search,
-        Fileupload,
-        Spinner
+        Search
     } from "flowbite-svelte" ;
     import mock_data from "../mock_data.js";
 
-    let upload_state = "";
-    let annotations_file = null;
-    let fileuploadprops = {
-        id: "annotations_file_input",
-        accept: ".pdf,application/pdf"
-    };
-
-    function file_upload() {
-        if (upload_state === "uploading" || upload_state === "done") return;
-
-        const input = document.getElementById(fileuploadprops.id);
-        const file = input.files[0];
-
-        // upload file here
-        console.log(file);
-
-        upload_state = "uploading";
-        setTimeout(() => {
-            upload_state = Math.random() < 0.5 ? "failed" : "done";
-        }, 2200);
-    }
-
     export let error = null;
     export let category;
-    let entries = null;
     export let show = false;
     export let hide = () => {
         /* NOP */
@@ -52,10 +27,25 @@
     }
 
     let query = "";
-
+    export let entries = null;
     let users = mock_data.users;
     let reviewers = [];
-    let files;
+    let authors;
+    let name;
+    let open_slots;
+    let category_id = category.id;
+    let fileInput;
+    let file;
+    let new_entry_id;
+
+    let categoryPath = `/api/categories/${category_id}/entries`;
+    let entryPath;
+    let requestPath;
+
+    function updatePaths() {
+        entryPath = `/api/categories/${category_id}/entries/${new_entry_id}/process`;
+        requestPath = `/api/categories/${category_id}/entries/${new_entry_id}/process/requests`;
+    }
 
     function apply_query(q) {
         query = q;
@@ -70,15 +60,6 @@
         }
     }
 
-    let authors;
-    let name;
-    let category_id = category.id;
-    let category_path = `/api/categories/${category_id}/entries`
-
-    let fileInput;
-    let file;
-    let new_entry_id;
-
     function createEntry() {
         file = fileInput.files[0];
         console.log(file);
@@ -86,9 +67,8 @@
         formData.append("authors", authors);
         formData.append("name", name);
         formData.append("file", file);
-        return fetch(category_path, {
+        return fetch(categoryPath, {
             method: "POST",
-
             body: formData
         })
             .then(resp => resp.json())
@@ -100,18 +80,68 @@
                     try {
                         hide();
                         new_entry_id = resp.content.id;
+                        updatePaths();
                     } catch (error) {
                         console.log(error);
                     }
-                    sendDirectRequest()
                 }
             })
             .catch(err => console.log(err));
     }
 
-    function sendDirectRequest() {
-
+    function processSubmission() {
+        createEntry();
+        createDirectRequestProcess();
+        reviewers.map(reviewer => createDirectRequest(reviewer.id()))
     }
+
+    function createDirectRequestProcess() {
+        let data = {
+            open_slots: reviewers.length
+        }
+
+        return fetch(entryPath, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(data)
+        })
+            .then(resp => resp.json())
+            .then(resp => {
+                if (resp.status < 200 || resp.status >= 300) {
+                    error = "" + resp.status + ": " + resp.message;
+                    console.log(error);
+                } else {
+
+                }
+            })
+            .catch(err => console.log(err));
+    }
+
+    function createDirectRequest(reviewer_id) {
+        let data = {
+            reviewer_id: reviewer_id
+        }
+        return fetch(requestPath, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(data)
+        })
+            .then(resp => resp.json())
+            .then(resp => {
+                if (resp.status < 200 || resp.status >= 300) {
+                    error = "" + resp.status + ": " + resp.message;
+                    console.log(error);
+                } else {
+
+                }
+            })
+            .catch(err => console.log(err));
+    }
+
 </script>
 
 <Modal bind:open={show} on:hide={() => hide ? hide() : null} permanent={true} size="lg">
@@ -123,7 +153,7 @@
                      on:click={hide}/>
     </svelte:fragment>
 
-    <form class="grid gap-y-6" enctype="multipart/form-data" on:submit|preventDefault={() => createEntry()}>
+    <form class="grid gap-y-6" enctype="multipart/form-data" on:submit|preventDefault={() => processSubmission()}>
         <div class="flex flex-row justify-between items-center">
             <Heading size="md" tag="h4"> Enter Paper Title</Heading>
             <input bind:value={name} class="min-w-[13.5rem] w-full rounded-lg" id=entered_entry_title required
@@ -135,51 +165,16 @@
                    placeholder="(Optional)" type=text>
         </div>
 
-
         <div class="flex flex-row justify-between items-center">
             <input bind:this={fileInput} type="file"/>
         </div>
 
-
-        <!--
-        <div class="flex flex-1 items-center ">
-            <Fileupload {...fileuploadprops} bind:value={annotations_file} inputClass="my-auto annotations_file_input"
-                        on:change={() => upload_state = ""}
-                        size="lg"
-            />
-            <Button class="m-auto"
-                    color={upload_state === "done" ? "green" : (upload_state === "failed" ? "red" : "blue")}
-                    disabled={!annotations_file} on:click={file_upload}
-                    outline size="md">
-                {#if upload_state === "uploading"}
-                    <Spinner class="mr-3" size="5"/>
-                {:else }
-                    {#if upload_state === "done"}
-                        <svg class="mr-3" width="15px" height="15px" viewBox="0 0 15 15" fill="none"
-                             xmlns="http://www.w3.org/2000/svg">
-                            <path d="M1 7L5.5 11.5L14 3" stroke="black" stroke-linecap="square"/>
-                        </svg>
-                    {:else }
-                        {#if upload_state === "failed" }
-                            <svg class="mr-3" width="15px" height="15px" xmlns="http://www.w3.org/2000/svg" x="0px"
-                                 y="0px"
-                                 viewBox="0 0 490 490" style="enable-background:new 0 0 490 490;" xml:space="preserve">
-                <polygon points="456.851,0 245,212.564 33.149,0 0.708,32.337 212.669,245.004 0.708,457.678 33.149,490 245,277.443 456.851,490
-                  489.292,457.678 277.331,245.004 489.292,32.337 "/>
-          </svg>
-                        {/if}
-                    {/if}
-                {/if}{upload_state === "done" ? "Done" : (upload_state === "failed" ? "Failed" : "Upload")}
-            </Button>
-        </div>
-        -->
-
-
         <div class="flex flex-row justify-between items-center">
             <Heading size="md" tag="h4">Choose The Number Of Review Slots</Heading>
-            <input class="justify-end" id=selected_open_slots min={ reviewers.length > 0 ? reviewers.length : 1 }
-                   type=number
-                   value=1>
+            <input class="justify-end rounded-lg"
+                   id=selected_open_slots
+                   min={reviewers.length > 0 ? reviewers.length : 1}
+                   type=number>
         </div>
 
         <Button color="primary">
@@ -213,7 +208,7 @@
                         <TableBodyCell>
                             <div class="flex flex-wrap items-center gap-2">
                                 <Button pill class="!p-2" outline color="red"
-                                        on:click={() => reviewers = reviewers.filter(e => e !== r)}>
+                                        on:click={() => {reviewers = reviewers.filter(e => e !== r)}}>
                                     <svg class="w-5 h-5" xmlns="http://www.w3.org/2000/svg" x="0px" y="0px"
                                          width="32px" height="32px" viewBox="0 0 64 64"
                                          xml:space="preserve">
@@ -237,7 +232,7 @@
         </Table>
 
     </div>
-    <Button color="primary" on:click={() => createEntry()} size="sm" type="submit">
+    <Button color="primary" on:click={() => processSubmission()} size="sm" type="submit">
         Finish Submission
     </Button>
 
