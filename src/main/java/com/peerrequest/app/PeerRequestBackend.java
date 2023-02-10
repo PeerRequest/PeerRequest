f39a0b5a3ba9ead0e0f74c1f6975d358c94f8689
Wil-Lee <mildt.michael@gmail.com>
2023-02-10 23:56:37 +0100

    checkstyle

--- src/main/java/com/peerrequest/app/PeerRequestBackend.java
+++ src/main/java/com/peerrequest/app/PeerRequestBackend.java
@@ -1,12 +1,9 @@
 package com.peerrequest.app;
 
 import com.peerrequest.app.services.NotificationService;
-import com.peerrequest.app.services.messages.ReviewMessageTemplates;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.event.ApplicationReadyEvent;
-import org.springframework.context.event.EventListener;
 
 /**
  * Main class for the PeerRequest Backend.
