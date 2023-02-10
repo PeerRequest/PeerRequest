d4c23d48d1bd3575cff4a69c539cf487ab301818
upxee <upxee@student.kit.edu>
2023-02-10 22:00:46 +0100

    adjust Review.svelte and Reviewers.svelte

--- frontend/src/components/Review.svelte
+++ frontend/src/components/Review.svelte
@@ -1,36 +1,50 @@
 <script>
     import {BreadcrumbItem, TableBodyCell, TableBodyRow} from "flowbite-svelte";
+    import Skeleton from "svelte-skeleton-loader"
 
-    export let href;
-    export let id;
+
+    export let review;
     export let category = null;
-    export let reviewer = null;
+
+    //TODO: UserController
+    export let reviewer = "Chihiro Kaori";
     export let paper = null;
+    export let loading = false;
 
 </script>
 
 
 <TableBodyRow>
-    <TableBodyCell>
-        <BreadcrumbItem href={href}>Review #{id}</BreadcrumbItem>
-    </TableBodyCell>
-
-    {#if reviewer !== null}
+    {#if loading}
+        {#each [...Array(5).keys()] as i}
+            <TableBodyCell>
+                <div>
+                    <Skeleton/>
+                </div>
+            </TableBodyCell>
+        {/each}
+    {:else }
         <TableBodyCell>
-            <BreadcrumbItem>{reviewer}</BreadcrumbItem>
+            <BreadcrumbItem href="/categories/{category.id}/entries/{paper.id}/reviews/{review.id}" >Review #{review.id}</BreadcrumbItem>
         </TableBodyCell>
-    {/if}
 
-    {#if paper !== null}
-        <TableBodyCell>
-            <BreadcrumbItem href="/categories/{category.id}/entries/{paper.id}">{paper.title}</BreadcrumbItem>
-        </TableBodyCell>
-    {/if}
+        {#if reviewer !== null}
+            <TableBodyCell>
+                <BreadcrumbItem>{reviewer}</BreadcrumbItem>
+            </TableBodyCell>
+        {/if}
 
-    {#if category !== null}
-        <TableBodyCell>
-            <BreadcrumbItem href="/categories/{category.id}">{category.name}</BreadcrumbItem>
-        </TableBodyCell>
+        {#if paper !== null}
+            <TableBodyCell>
+                <BreadcrumbItem href="/categories/{category.id}/entries/{paper.id}">{paper.name}</BreadcrumbItem>
+            </TableBodyCell>
+        {/if}
+
+        {#if category !== null}
+            <TableBodyCell>
+                <BreadcrumbItem href="/categories/{category.id}">{category.name}</BreadcrumbItem>
+            </TableBodyCell>
+        {/if}
     {/if}
 
 </TableBodyRow>
