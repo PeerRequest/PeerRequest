54295aa988cc8de025767cb5028278eebe0b7720
veropossumus <16821784+veropossumus@users.noreply.github.com>
2023-02-11 23:45:58 +0100

    replace mock in Review.svelte with correct reviewer name

--- frontend/src/components/Review.svelte
+++ frontend/src/components/Review.svelte
@@ -1,18 +1,53 @@
 <script>
     import {BreadcrumbItem, TableBodyCell, TableBodyRow} from "flowbite-svelte";
     import Skeleton from "svelte-skeleton-loader"
+    import {onMount} from "svelte";
 
+    export let error;
     export let review;
     export let category = null;
 
     //TODO: UserController
-    export let reviewer = "Chihiro Kaori";
+
     export let paper = null;
     export let loading = false;
 
     export let show_category = false;
     export let show_paper = false;
     export let show_reviewer = false;
+
+
+    let users = null;
+    let reviewer = null;
+
+    function loadReviewer() {
+        loadUsers();
+        if (users === !null) {
+            reviewer = users.find(user => user.id === review.reviewer_id)
+        } else {
+            console.log("No users could be found.")
+        }
+
+    }
+
+    function loadUsers() {
+        users = null;
+        fetch("/api/users")
+            .then(resp => resp.json())
+            .then(resp => {
+                if (resp.status < 200 || resp.status >= 300) {
+                    error = "" + resp.status + ": " + resp.message;
+                    console.log(error);
+                } else {
+                    users = resp.content;
+                }
+            })
+            .catch(err => console.log(err))
+    }
+
+    onMount(() => {
+        loadReviewer();
+    });
 
 </script>
 
@@ -28,12 +63,15 @@
         {/each}
     {:else }
         <TableBodyCell>
-            <BreadcrumbItem href="/categories/{category.id}/entries/{paper.id}/reviews/{review.id}" >Review #{review.id}</BreadcrumbItem>
+            <BreadcrumbItem href="/categories/{category.id}/entries/{paper.id}/reviews/{review.id}">Review
+                #{review.id}</BreadcrumbItem>
         </TableBodyCell>
 
         {#if show_reviewer}
             <TableBodyCell>
-                <BreadcrumbItem>{reviewer}</BreadcrumbItem>
+                <BreadcrumbItem>
+                    {reviewer === !null ? ((reviewer.name === !null) ? reviewer.name : "undefined") : "no reviewer"}
+                </BreadcrumbItem>
             </TableBodyCell>
         {/if}
 
