eb783e9f89c68a18c9eb7925f69454dbcdd9d88d
veropossumus <16821784+veropossumus@users.noreply.github.com>
2023-02-12 00:16:40 +0100

    integrate my-papers

--- frontend/src/routes/my-papers/+page.svelte
+++ frontend/src/routes/my-papers/+page.svelte
@@ -5,9 +5,32 @@
     import ResponsiveBreadCrumb from "../../components/ResponsiveBreadCrumb.svelte";
     import Papers from "../../components/Papers.svelte";
     import Paper from "../../components/Paper.svelte";
+    import {onMount} from "svelte";
 
     const user = mock_data.users[23];
     const papers = mock_data.papers;
+
+    export let error;
+    let entries = null;
+
+    function loadUserEntries() {
+        entries = null;
+        fetch("/api/entries")
+            .then(resp => resp.json())
+            .then(resp => {
+                if (resp.status < 200 || resp.status >= 300) {
+                    error = "" + resp.status + ": " + resp.message;
+                    console.log(error);
+                } else {
+                    entries = resp.content;
+                }
+            })
+            .catch(err => console.log(err))
+    }
+
+    onMount(() => {
+        loadUserEntries();
+    });
 </script>
 
 <svelte:head>
@@ -21,10 +44,9 @@
     </ResponsiveBreadCrumb>
     <Heading class="mb-4" tag="h2">My Papers</Heading>
 
-    <Papers
-            show_category=true
-    >
-        {#each papers.filter((p) => p.researcher === user.name) as p}
+    <Papers show_category=true>
+
+        {#each entries !== null as p}
             <Paper
                     href="/categories/{p.category.id}/{p.id}"
                     paper={p}
