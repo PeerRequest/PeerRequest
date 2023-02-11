6336b45f759892910ba8b9813e3a4d6824cab0a5
upxee <upxee@student.kit.edu>
2023-02-11 11:09:44 +0100

    integrate getMessages

--- frontend/src/components/MessageBoard.svelte
+++ frontend/src/components/MessageBoard.svelte
@@ -1,16 +1,28 @@
 <script>
-    import mock_data from "../mock_data.js";
     import Comment from './Comment.svelte'
     import {Button, Chevron, Dropdown, DropdownItem} from "flowbite-svelte";
-    import {afterUpdate} from "svelte";
+    import {afterUpdate, onMount} from "svelte";
+    import Error from "./Error.svelte";
 
-    let sortedComments = mock_data.comments
-    let amount = sortedComments.length
+
+    export let error = null;
+    export let review = {
+        id: "",
+        entry_id:""
+    }
+    export let category = {
+        id: ""
+    }
+    let sortedComments = null
+    let amount = 0
     let order = true, comment
 
-    const handleOrder = (data) => {
-        if (order) return sortedComments = data.sort((a, b) => new Date(a.date) - new Date(b.date))
-        return sortedComments = data.sort((a, b) => new Date(b.date) - new Date(a.date))
+
+
+
+    const handleOrder = (sorting_data) => {
+        if (order) return sortedComments = sorting_data.sort((a, b) => new Date(a.date) - new Date(b.date))
+        return sortedComments = sorting_data.sort((a, b) => new Date(b.date) - new Date(a.date))
     };
 
     afterUpdate(() => {
@@ -45,30 +57,55 @@
         comment = ""
         amount = sortedComments.length
     }
+
+    function loadComments() {
+        sortedComments = null;
+        fetch("/api/categories/" + category.id + "/entries/" + review.entry_id + "/reviews/" + review.id + "/messages")
+            .then(resp => resp.json())
+            .then(resp => {
+                if (resp.status < 200 || resp.status >= 300) {
+                    error = "" + resp.status + ": " + resp.message;
+                    console.log(error);
+                } else {
+                    sortedComments = resp;
+                }
+            })
+            .catch(err => console.log(err))
+    }
+
+    onMount(() => {
+        loadComments()
+    })
 </script>
 
+{#if error !== null}
+    <Error error={error}/>
+{:else}
+    {#if (sortedComments === null)}
+        LOADING
+    {:else}
+        <main class="flex grid grid-cols-1 justify-center">
+            <header class="flex">
+                <h1 class="font-bold text-sm my-2">{amount}{amount > 1 ? " comments" : " comment" }</h1>
+            </header>
+            <Button class="w-44 h-8">
+                <Chevron> Sort by {order ? "Oldest" : "Newest"}</Chevron>
+            </Button>
+            <Dropdown>
+                <DropdownItem on:click={()=>order = !order}
+                              on:click={handleOrder(sortedComments)}>{order ? "Newest" : "Oldest"}</DropdownItem>
+            </Dropdown>
 
-<main class="flex grid grid-cols-1 justify-center">
-    <header class="flex">
-        <h1 class="font-bold text-sm my-2">{amount}{amount > 1 ? " comments" : " comment" }</h1>
-    </header>
-    <Button class="w-44 h-8">
-        <Chevron> Sort by {order ? "Oldest" : "Newest"}</Chevron>
-    </Button>
-    <Dropdown>
-        <DropdownItem on:click={()=>order = !order}
-                      on:click={handleOrder(sortedComments)}>{order ? "Newest" : "Oldest"}</DropdownItem>
-    </Dropdown>
+            <div class="max-h-[34vh] h-screen w-full overflow-y-auto my-4 " id="CommentSection">
 
-    <div class="max-h-[34vh] h-screen w-full overflow-y-auto my-4 " id="CommentSection">
+                {#each sortedComments as data}
+                    <Comment bind:data={data}/>
+                {/each}
 
-        {#each sortedComments as data}
-            <Comment data={data}/>
-        {/each}
-
-    </div>
-    <form on:submit={submitComment}>
-        <input bind:value={comment} class="w-full rounded-lg" id="input-text" placeholder="Enter comment" type="text">
-    </form>
-</main>
-
+            </div>
+            <form on:submit={submitComment}>
+                <input bind:value={comment} class="w-full rounded-lg" id="input-text" placeholder="Enter comment" type="text">
+            </form>
+        </main>
+    {/if}
+{/if}
