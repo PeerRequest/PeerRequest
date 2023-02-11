f34d609c45a6cc75835804704e95cf55b81df27d
Wil-Lee <mildt.michael@gmail.com>
2023-02-11 12:57:08 +0100

    remove duplicate deleteDirectRequest method

--- src/main/java/com/peerrequest/app/api/DirectRequestsController.java
+++ src/main/java/com/peerrequest/app/api/DirectRequestsController.java
@@ -173,28 +173,6 @@ public class DirectRequestsController extends ServiceBasedController {
         return deleted.map(DirectRequest::toDto);
     }
 
-    @DeleteMapping("/{category_id}/entries/{entry_id}/process/requests/{request_id}")
-    DirectRequest.Dto deleteDirectRequest(@PathVariable("request_id") Long id,
-                                          @AuthenticationPrincipal OAuth2User user) {
-        var option = this.directRequestService.get(id);
-        if (option.isEmpty()) {
-            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "category does not exist");
-        }
-        var directRequestProcess = this.directRequestProcessService.get(option.get().getDirectRequestProcessId());
-        if (directRequestProcess.isEmpty()) {
-            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "process does not exist");
-        }
-
-        var researcherId = this.entryService.get(directRequestProcess.get().getEntryId()).get().getResearcherId();
-
-        if (!researcherId.equals(user.getAttribute("sub"))) {
-            throw new ResponseStatusException(HttpStatus.FORBIDDEN, "only the researcher may delete the request");
-        }
-
-        var deleted = this.directRequestService.delete(id);
-        return deleted.map(DirectRequest::toDto).get();
-    }
-
     /**
      * Gets the list of all requests of an entry.
      *
