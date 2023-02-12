bef89f965c133ee618b997e762503390367c17cc
veropossumus <16821784+veropossumus@users.noreply.github.com>
2023-02-12 17:06:21 +0100

    add tables for pending and accepted requests on homepage

---
+++ frontend/src/components/Requests.svelte
@@ -0,0 +1,30 @@
+<script>
+    import {Table, TableBody, TableHead, TableHeadCell} from "flowbite-svelte";
+
+    export let accepted = false;
+    export let pending = false;
+</script>
+
+<Table noborder={false} shadow={true}>
+    <TableHead>
+
+        {#if accepted}
+            <TableHeadCell>Accepted Requests</TableHeadCell>
+        {:else}
+            {#if pending}
+                <TableHeadCell>Pending Requests</TableHeadCell>
+
+            {:else}
+                <TableHeadCell>Requests</TableHeadCell>
+            {/if}
+        {/if}
+
+        {#if pending}
+            <TableHeadCell></TableHeadCell>
+        {/if}
+
+    </TableHead>
+    <TableBody class="divide-y">
+        <slot></slot>
+    </TableBody>
+</Table>
