9f38b44a6fed8036249b885c824c401405429982
veropossumus <16821784+veropossumus@users.noreply.github.com>
2023-02-12 17:06:21 +0100

    add tables for pending and accepted requests on homepage

---
+++ frontend/src/components/Request.svelte
@@ -0,0 +1,28 @@
+<script>
+    import {
+        BreadcrumbItem, Button,
+        TableBodyCell,
+        TableBodyRow
+    } from "flowbite-svelte";
+
+
+    export let request;
+
+    export let pending = false;
+    export let accepted = false;
+
+</script>
+
+<TableBodyRow>
+    <TableBodyCell>
+        <BreadcrumbItem>this is a request</BreadcrumbItem>
+
+
+    </TableBodyCell>
+    {#if pending}
+        <TableBodyCell>
+            <Button outline size="sm" href="">Accept</Button>
+            <Button outline color="red" size="sm" href="">Decline</Button>
+        </TableBodyCell>
+    {/if}
+</TableBodyRow>
--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -1,13 +1,80 @@
 <script>
 
-  import { BreadcrumbItem, Heading } from "flowbite-svelte";
-  import Container from "../components/Container.svelte";
-  import ResponsiveBreadCrumb from "../components/ResponsiveBreadCrumb.svelte";
+    import {BreadcrumbItem, Heading} from "flowbite-svelte";
+    import Container from "../components/Container.svelte";
+    import ResponsiveBreadCrumb from "../components/ResponsiveBreadCrumb.svelte";
+    import {onMount} from "svelte";
+    import Requests from "../components/Requests.svelte";
+    import Request from "../components/Request.svelte";
+
+    export let error;
+
+    let requests = [];
+    let pending_requests = [];
+    let accepted_requests = [];
+
+
+    function loadRequests() {
+        requests = [];
+
+        fetch("/api/requests")
+            .then(resp => resp.json())
+            .then(resp => {
+                if (resp.status < 200 || resp.status >= 300) {
+                    error = "" + resp.status + ": " + resp.message;
+                    console.log(error);
+                } else {
+                    requests = resp.content;
+                }
+            })
+            .catch(err => console.log(err))
+    }
+
+
+    function filterRequests() {
+        if (requests !== null) {
+            pending_requests = requests.filter(request => request.state === "PENDING")
+            accepted_requests = requests.filter(request => request.state === "ACCEPTED")
+        } else {
+            console.log("No requests found.")
+        }
+    }
+
+
+    onMount(() => {
+        loadRequests()
+        filterRequests()
+    });
 </script>
 
 <Container>
-  <ResponsiveBreadCrumb>
-    <BreadcrumbItem home href="/">Home</BreadcrumbItem>
-  </ResponsiveBreadCrumb>
-  <Heading class="mb-4" tag="h2">Home</Heading>
+    <ResponsiveBreadCrumb>
+        <BreadcrumbItem home href="/">Home</BreadcrumbItem>
+    </ResponsiveBreadCrumb>
+    <Heading class="mb-4" tag="h2">Home</Heading>
+
+
+    <div class="justify-center flex w-full gap-x-4">
+        <Requests
+                pending=true
+        >
+            {#each pending_requests as pr}
+                <Request
+                        request={pr}
+                        pending=true
+                />
+            {/each}
+        </Requests>
+        <Requests
+                accepted=true
+        >
+            {#each accepted_requests as ar}
+                <Request
+                        request={ar}
+                        accepted=true
+                />
+            {/each}
+        </Requests>
+    </div>
+
 </Container>
