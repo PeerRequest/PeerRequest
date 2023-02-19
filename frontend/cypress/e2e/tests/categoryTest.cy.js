e4af03c05512c0c69b2e964df229cb0def11c504
Nikola <NoMail>
2023-02-19 11:13:43 +0100

    add aria-label

---
+++ frontend/cypress/e2e/tests/categoryTest.cy.js
@@ -0,0 +1,18 @@
+describe("Create category", () => {
+
+    it("Logging in", () => {
+        cy.visit(Cypress.env('auth_base_url') +
+            "/realms/" + Cypress.env('auth_realm') +
+            "/protocol/openid-connect/auth?response_type=code" +
+            "&client_id=" + Cypress.env('auth_client_id') +
+            "&scope=openid&redirect_uri=http://localhost:8080/login/oauth2/code/keycloak")
+            .get('input[name="username"]').type(Cypress.env("test5-user").username)
+            .get('input[name="password"]').type(Cypress.env("test5-user").password)
+            .get('input[name="login"]').click()
+            .wait(300)
+            .origin("http://localhost:8080", () => {
+                cy.get('a[href="/categories"]').click()
+                    .get('button[aria-label="create-new-conference"').click()
+            })
+    })
+})
