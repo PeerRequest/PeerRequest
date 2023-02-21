a96fcd2c1dff18207f03165df3da11659d5482f0
upxee <upxee@student.kit.edu>
2023-02-21 17:17:50 +0100

    replace alert with toast in request

--- frontend/src/components/Request.svelte
+++ frontend/src/components/Request.svelte
@@ -2,7 +2,8 @@
     import {
         BreadcrumbItem, Button,
         TableBodyCell,
-        TableBodyRow
+        TableBodyRow,
+        Toast
     } from "flowbite-svelte";
     import {createEventDispatcher} from "svelte";
 
@@ -12,9 +13,26 @@
     export let accepted = false;
     export let error;
 
+    let show_request_notification = false;
+    let counter = 6;
+    let request_state = ""
+
+    function triggerNotification() {
+        show_request_notification = true;
+        counter = 6;
+        timeout();
+    }
+    function timeout() {
+        if (--counter > 0)
+            return setTimeout(timeout, 1000);
+        show_request_notification = false;
+    }
+
     const dispatch = createEventDispatcher();
 
     function updateRequest(state) {
+        request_state = state;
+        triggerNotification()
         let data = {
             state: state
         };
@@ -32,7 +50,6 @@
                     console.log(error);
                 } else {
                     dispatch("requestUpdated", state);
-                    alert("You have "+ state.toLocaleLowerCase() +" the review request")
                 }
             })
             .catch(err => console.log(err))
@@ -40,6 +57,23 @@
 
 </script>
 
+{#if request_state === "ACCEPTED"}
+    <Toast color="green" class="mb-2 fixed max-w-fit w-[40vw] top-0 right-[35vw]" bind:open={show_request_notification}>
+        <svelte:fragment slot="icon">
+            <svg aria-hidden="true" class="w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z" clip-rule="evenodd"></path></svg>
+            <span class="sr-only">Check icon</span>
+        </svelte:fragment>
+        You have accepted the review request
+    </Toast>
+{:else }
+    <Toast color="red" class="mb-2 fixed max-w-fit w-[40vw] top-0 right-[35vw]" bind:open={show_request_notification}>
+        <svelte:fragment slot="icon">
+            <svg aria-hidden="true" class="w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
+            <span class="sr-only">Error icon</span>
+        </svelte:fragment>
+        You have declined the review request
+    </Toast>
+{/if}
 <TableBodyRow>
     <TableBodyCell>
         <BreadcrumbItem href="categories/{entry.category_id}/entries/{entry.id}">{entry.name}</BreadcrumbItem>
