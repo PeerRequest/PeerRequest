a3dbe11972b677e8a1f060f4b9d6413d9ec2dbd9
Wil-Lee <mildt.michael@gmail.com>
2023-03-12 11:51:18 +0100

    create test data

---
+++ src/test/java/com/peerrequest/app/api/DirectRequestsControllerTest.java
@@ -0,0 +1,294 @@
+package com.peerrequest.app.api;
+
+import com.peerrequest.app.PeerRequestBackend;
+import com.peerrequest.app.data.*;
+import com.peerrequest.app.services.*;
+import org.apache.commons.io.FileUtils;
+import org.junit.jupiter.api.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mock.web.MockHttpSession;
+import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.util.ResourceUtils;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+import java.util.concurrent.ThreadLocalRandom;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+/**
+ * This test class tests the endpoints of the {@link EntriesController} class.
+ */
+@SpringBootTest(properties = {"spring.mail.from=mail", "spring.load=false"})
+@AutoConfigureMockMvc
+@ContextConfiguration(classes = PeerRequestBackend.class)
+@ActiveProfiles("test")
+@DirtiesContext(classMode = DirtiesContext.ClassMode.BEFORE_CLASS)
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+public class DirectRequestsControllerTest {
+
+    @MockBean
+    ClientRegistrationRepository clientRegistrationRepository;
+
+    @MockBean
+    JavaMailSender mailSender;
+
+    @Autowired
+    private MockMvc mockMvc;
+    private static Category category;
+    //for getSpecificRequests()
+    private static List<Entry> entries;
+    private static List<DirectRequestProcess> drps;
+    private static List<DirectRequest> directRequests;
+    private static UUID userId = UUID.randomUUID();
+
+    private static MockHttpSession session;
+
+    // should be multiple of four
+    private static int currentUserEntrySize = 20;
+
+    private static Entry userEntryRequests;
+
+    @BeforeAll
+    static void setUp(@Autowired CategoryService categoryService, @Autowired EntryService entryService,
+                      @Autowired DocumentService documentService, @Autowired DirectRequestProcessService drpService,
+                      @Autowired DirectRequestService directRequestService,
+                      @Autowired MockMvc mockMvc) throws Exception {
+        // login and set current user
+        session = new MockHttpSession();
+        mockMvc.perform(
+                        get("/test/auth/login")
+                                .queryParam("user_id", userId.toString())
+                                .queryParam("user_name", "ich")
+                                .queryParam("given_name", "kann")
+                                .queryParam("family_name", "das")
+                                .queryParam("email", "alles@nicht.mehr")
+                                .session(session)
+                                .secure(true))
+                .andExpect(status().isOk());
+
+        // setup data
+        category = categoryService.create(
+                Category.builder()
+                        .name("Test Category")
+                        .year(2000)
+                        .label(Category.CategoryLabel.INTERNAL)
+                        .minScore(0)
+                        .maxScore(5)
+                        .researcherId(userId.toString())
+                        .build().toDto());
+
+        File file = ResourceUtils.getFile("classpath:loremipsum.pdf");
+        Document.Dto document = new Document.Dto(Optional.empty(), Optional.of(FileUtils.readFileToByteArray(file)),
+                Optional.of("loremipsum"));
+
+
+        // create entry, drp, and 120 Request (40 each state, random open slots 0-10) for currently signed-in user
+        // to list the requests
+        userEntryRequests = Entry.builder()
+                .researcherId(userId.toString())
+                .name("User Entry with Requests")
+                .authors("Alan Turing")
+                .documentId(documentService.create(document).getId())
+                .categoryId(category.getId())
+                .build();
+        entryService.create(userEntryRequests.toDto());
+        entries.add(userEntryRequests);
+
+        var userEntryRequestsDrp = drpService.create(
+                DirectRequestProcess.builder()
+                        .entryId(userEntryRequests.getId())
+                        .openSlots(ThreadLocalRandom.current().nextInt(0, 11))
+                        .build().toDto());
+        drpService.create(userEntryRequestsDrp.toDto());
+
+        for (int i = 0; i < 120; i++) {
+            var r = directRequestService.create(
+                    DirectRequest.builder()
+                            .state( i < 40 ? DirectRequest.RequestState.PENDING
+                                    : (i < 80 ? DirectRequest.RequestState.ACCEPTED
+                                    : DirectRequest.RequestState.DECLINED))
+                            .reviewerId(userId.toString())
+                            .directRequestProcessId(userEntryRequestsDrp.getId())
+                            .build().toDto());
+        }
+
+        // create entry drp of user with 9 requests (each state 3 times) - to delete a request
+        var userEntryDelete = Entry.builder()
+                .researcherId(userId.toString())
+                .name("User Entry Delete")
+                .authors("Alan Turing")
+                .documentId(documentService.create(document).getId())
+                .categoryId(category.getId())
+                .build();
+        entryService.create(userEntryDelete.toDto());
+        entries.add(userEntryDelete);
+
+        var userEntryDeleteDrp = drpService.create(
+                DirectRequestProcess.builder()
+                        .entryId(userEntryDelete.getId())
+                        .openSlots(ThreadLocalRandom.current().nextInt(0, 11))
+                        .build().toDto());
+        drpService.create(userEntryRequestsDrp.toDto());
+
+        for (int i = 0; i < 9; i++) {
+            var r = directRequestService.create(
+                    DirectRequest.builder()
+                            .state( i < 3 ? DirectRequest.RequestState.PENDING
+                                    : (i < 6 ? DirectRequest.RequestState.ACCEPTED
+                                    : DirectRequest.RequestState.DECLINED))
+                            .reviewerId(userId.toString())
+                            .directRequestProcessId(userEntryDeleteDrp.getId())
+                            .build().toDto()
+            );
+        }
+
+
+        // create entry of user without drp - to create a drp
+        var userEntryCreateDrp = Entry.builder()
+                .researcherId(userId.toString())
+                .name("User Entry Create Direct Request Process")
+                .authors("Alan Turing")
+                .documentId(documentService.create(document).getId())
+                .categoryId(category.getId())
+                .build();
+        entryService.create(userEntryCreateDrp.toDto());
+        entries.add(userEntryCreateDrp);
+
+
+        // create three entries, drps with open slot but no requests for currently signed-in user - to claim open slots
+        for (int i = 0; i < 9; i++) {
+            var e = entryService.create(
+                    Entry.builder()
+                            .researcherId(UUID.randomUUID().toString())
+                            .name("Not User Entry with Open Slots " + i)
+                            .authors("Alan Turing")
+                            .documentId(documentService.create(document).getId())
+                            .categoryId(category.getId())
+                            .build().toDto());
+            entries.add(e);
+
+            var p = drpService.create(
+                    DirectRequestProcess.builder()
+                            .entryId(e.getId())
+                            .openSlots(10)
+                            .build().toDto());
+            drpService.create(p.toDto());
+        }
+
+
+        // create three entries, drps with 3 requests each (first drp has pending, second accepted, third declined)
+        // - to patch direct requests
+        for (int i = 0; i < 9; i++) {
+
+            var e = entryService.create(
+                    Entry.builder()
+                    .researcherId(userId.toString())
+                    .name("User Entry Requested " + i)
+                    .authors("Alan Turing")
+                    .documentId(documentService.create(document).getId())
+                    .categoryId(category.getId())
+                    .build().toDto());
+
+            var p = drpService.create(
+                    DirectRequestProcess.builder()
+                    .entryId(e.getId())
+                    .openSlots(0)
+                    .build().toDto());
+            drpService.create(p.toDto());
+
+            var r = directRequestService.create(
+                    DirectRequest.builder()
+                            .state( i < 3 ? DirectRequest.RequestState.PENDING
+                                    : (i < 6 ? DirectRequest.RequestState.ACCEPTED
+                                    : DirectRequest.RequestState.DECLINED))
+                            .reviewerId(userId.toString())
+                            .directRequestProcessId(p.getId())
+                            .build().toDto()
+            );
+
+        }
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    @Order(1)
+    void getDirectRequestProcess() throws Exception {
+
+    }
+
+    @Test
+    @Order(1)
+    void createDirectRequestProcess() throws Exception {
+
+    }
+
+    @Test
+    @Order(1)
+    void patchDirectRequestProcess() throws Exception {
+
+    }
+
+    @Test
+    @Order(1)
+    void getDirectRequest() throws Exception {
+
+    }
+
+    @Test
+    @Order(1)
+    void deleteDirectRequest() throws Exception {
+
+    }
+
+    @Test
+    @Order(1)
+    void listDirectRequestsByEntry() throws Exception {
+
+    }
+
+    @Test
+    @Order(1)
+    void postDirectRequest() throws Exception {
+
+    }
+
+    @Test
+    @Order(1)
+    void patchDirectRequest() throws Exception {
+
+    }
+
+    @Test
+    @Order(1)
+    void listRequestsByResearcher() throws Exception {
+
+    }
+
+    @Test
+    @Order(1)
+    void getSpecificRequests() throws Exception {
+
+    }
+
+    @Test
+    @Order(1)
+    void claimOpenSlot() throws Exception {
+
+    }
+}
