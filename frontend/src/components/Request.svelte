636f6308c0fde83a3bd76b6258ef6e736102b825
upxee <upxee@student.kit.edu>
2023-03-13 17:26:54 +0100

    reposition Toasts from top to bottom

--- frontend/src/components/Request.svelte
+++ frontend/src/components/Request.svelte
@@ -92,7 +92,7 @@
 </script>
 
 {#if request_state === "ACCEPTED"}
-    <Toast color="green" class="mb-2 fixed max-w-fit w-[40vw] top-0 right-[35vw]" bind:open={show_request_notification}>
+    <Toast color="green" class="mb-2 fixed max-w-fit w-[40vw] bottom-0 right-[35vw]" bind:open={show_request_notification}>
         <svelte:fragment slot="icon">
             <svg aria-hidden="true" class="w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z" clip-rule="evenodd"></path></svg>
             <span class="sr-only">Check icon</span>
@@ -100,7 +100,7 @@
         You have accepted the review request
     </Toast>
 {:else }
-    <Toast color="red" class="mb-2 fixed max-w-fit w-[40vw] top-0 right-[35vw]" bind:open={show_request_notification}>
+    <Toast color="red" class="mb-2 fixed max-w-fit w-[40vw] bottom-0 right-[35vw]" bind:open={show_request_notification}>
         <svelte:fragment slot="icon">
             <svg aria-hidden="true" class="w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
             <span class="sr-only">Error icon</span>
--- frontend/src/components/ReviewForm.svelte
+++ frontend/src/components/ReviewForm.svelte
@@ -119,7 +119,7 @@
 
 </script>
 
-<Toast color="green" class="mb-2 absolute w-[20vw] top-0 right-[40vw]" bind:open={show_save_notification}>
+<Toast color="green" class="mb-2 absolute w-[20vw] bottom-0 right-[40vw]" bind:open={show_save_notification}>
     <svelte:fragment slot="icon">
         <svg aria-hidden="true" class="w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z" clip-rule="evenodd"></path></svg>
         <span class="sr-only">Check icon</span>
