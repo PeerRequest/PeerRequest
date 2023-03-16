f5ff791a433b2de7bba11fe35d6442bcb430c147
Nikola <NoMail>
2023-03-16 10:38:23 +0100

    add tests

--- frontend/cypress/e2e/categories_test.cy.ts
+++ frontend/cypress/e2e/categories_test.cy.ts
@@ -6,14 +6,54 @@ describe('Categories', () => {
             '&family_name=Mustermann&email=ichwill@nichtmehr.sob')
             .then((response) => {
                 const cookieString = response.headers["set-cookie"][0];
-                cy.log(JSON.stringify(response)).setCookie('JSESSIONID',
+                cy.setCookie('JSESSIONID',
                     cookieString.substring(11, cookieString.length - 18))
-            }).visit('http://localhost:8080')
+            })
+
+            .visit('http://localhost:8080')
+            .get('a[href="/categories"]').click()
+            .get('button[aria-label="Create Category"]').click()
+            .get('input[id="conference_name"]').type("First Conference")
+            .get('input[id="conference_year"').type("2023")
+            .get('input[value="INTERNAL"]').click()
+            .get('input[aria-label="min_score"]').type("1")
+            .get('input[aria-label="max_score"]').type("5")
+            .get('input[aria-label="score_step_size"]').type("1")
+            .get('button[type="submit"]').click()
+
+            .visit('http://localhost:8080')
+            .get('a[href="/categories"]').click()
+            .get('button[aria-label="Create Category"]').click()
+            .get('input[id="conference_name"]').type("Delete Conference")
+            .get('input[id="conference_year"').type("2023")
+            .get('input[value="INTERNAL"]').click()
+            .get('input[aria-label="min_score"]').type("1")
+            .get('input[aria-label="max_score"]').type("5")
+            .get('input[aria-label="score_step_size"]').type("1")
+            .get('button[type="submit"]').click()
+
+        cy.visit('http://localhost:8080')
     })
 
-    it('login', () => {
-        cy.get('a[href="/categories"]').click()
-        .get('button[id="avatar_with_name"]').click()
+    beforeEach(() => {
+        cy.visit('http://localhost:8080')
+            .get('a[href="/categories"]').click()
+    })
+
+    afterEach(() => {
+        cy.visit('http://localhost:8080')
+    })
+
+    it('create category', () => {
+        cy
+            .get('button[aria-label="Create Category"]').click()
+            .get('input[id="conference_name"]').type("Example Conference")
+            .get('input[id="conference_year"').type("2023")
+            .get('input[value="INTERNAL"]').click()
+            .get('input[aria-label="min_score"]').type("1")
+            .get('input[aria-label="max_score"]').type("5")
+            .get('input[aria-label="score_step_size"]').type("1")
+            .get('button[type="submit"]').click()
+            .get('a[href="/categories/3"]').should('be.visible')
     })
 })
-
